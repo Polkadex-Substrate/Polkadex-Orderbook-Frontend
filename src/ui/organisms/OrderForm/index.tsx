0d052286fec26108a04a458cb81dec4f27849b11
charleslukes <charleslukes28@gmail.com>
2021-09-29 00:58:17 +0100

    add form logic

--- src/ui/organisms/OrderForm/index.tsx
+++ src/ui/organisms/OrderForm/index.tsx
@@ -4,6 +4,8 @@ import { Button, TabHeader, Tabs } from "src/ui/components";
 import { OrderInput } from "src/ui/molecules";
 import { FormEvent, useState } from "react";
 import { useEffect } from "react";
+import { useP2P } from "src/ui/templates/PlaceOrder/useP2P";
+import { cleanPositiveFloatInput, precisionRegExp } from "src/helpers";
 
 export const OrderForm = () => {
   const [totalAmount, setTotalAmount] = useState<string | number>('');
@@ -11,8 +13,9 @@ export const OrderForm = () => {
     price: null,
     amount: null
   });
+  const {currentMarket} = useP2P()
 
-  useEffect(() => {
+  useEffect(() => {    
     if(orderInput.price && orderInput.amount){
       const total = Number(orderInput.price) * Number(orderInput.amount);
       setTotalAmount(total);
@@ -27,7 +30,22 @@ export const OrderForm = () => {
 
   const handleFormInput = (e: FormEvent<HTMLFormElement>) => {
     const {name, value} = e.target as any;
-    setOrderInput({...orderInput, [name]: value});
+    let precision = 4;
+    const convertedValue = cleanPositiveFloatInput(value);
+    if(name === 'amount') {
+      precision = currentMarket?.amount_precision;
+    }
+    else {
+      precision = currentMarket?.price_precision;
+    }
+    if(convertedValue.match(precisionRegExp(precision))){      
+      setOrderInput({...orderInput, [name]: convertedValue});
+    }
+  }
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // call the place order 
   }
 
   return (
@@ -42,10 +60,10 @@ export const OrderForm = () => {
           </TabHeader>
         </S.Header>
         <div>
-          <form onChange={handleFormInput} onSubmit={() => console.log("Submit..")}>
-            <OrderInput label="Price" name="price" placeholder="Price" />
-            <OrderInput label="Amount" name="amount" placeholder="Amount" />
-            <OrderInput label="Total" value={totalAmount} placeholder="Total" disabled />
+          <form onChange={handleFormInput} onSubmit={handleFormSubmit}>
+            <OrderInput label="Price" name="price" placeholder="Price" token={currentMarket?.base_unit.toUpperCase()} />
+            <OrderInput label="Amount" name="amount" placeholder="Amount" token={currentMarket?.quote_unit.toUpperCase()} />
+            <OrderInput label="Total" value={totalAmount} placeholder="Total" token={currentMarket?.base_unit.toUpperCase()} disabled />
             <Button
               title="Log in"
               style={{ width: "100%", justifyContent: "center" }}
