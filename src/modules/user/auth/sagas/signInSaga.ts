9145dcb41f654267be6444e654a0c8d10935b706
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2021-09-28 21:01:29 +0530

    added better type definition in signInSaga

--- src/modules/user/auth/sagas/signInSaga.ts
+++ src/modules/user/auth/sagas/signInSaga.ts
@@ -2,7 +2,7 @@ import { call, put } from "redux-saga/effects";
 import { keyring } from '@polkadot/ui-keyring';
 import { API, proxyRestUrl, RequestOptions } from "../../../../api";
 import { sendError } from "../../../";
-import { User, userData } from "../../profile";
+import { User, userData, UserSkeleton } from "../../profile";
 import {
   signInData,
   signInError,
@@ -20,7 +20,7 @@ export function* signInSaga(action: SignInFetch) {
   try {
     const polkadexWorker = (window as any).polkadexWorker
     const { address, password } = action.payload
-    const user = yield call(() => getKeyringPairFromAddress(address, password));
+    const user: User = yield call(() => getKeyringPairFromAddress(address, password));
     const authResponse = yield call(() => polkadexWorker.authenticate(user));
     console.log({ authResponse })
     yield put(userData({ user }));
@@ -44,11 +44,17 @@ export function* signInSaga(action: SignInFetch) {
   }
 }
 
-const getKeyringPairFromAddress = async (address: string, password: string) => {
+const getKeyringPairFromAddress = async (address: string, password: string): Promise<User> => {
   try {
     const userPair = keyring.getPair(address)
+    const account = keyring.getAccount(address)
     console.log(userPair.address, userPair.isLocked);
-    return userPair
+    return {
+      username: account.meta.name,
+      address: userPair.address,
+      password,
+      keyringPair: userPair
+    }
   } catch (e) {
     throw new Error(e);
   }
