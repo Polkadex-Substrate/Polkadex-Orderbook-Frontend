3671e58bd9a55f29dffb402176a3967faf265771
charleslukes <charleslukes28@gmail.com>
2021-09-29 21:45:31 +0100

    entry saga

---
+++ src/modules/user/cancelOrders/sagas/index.ts
@@ -0,0 +1,7 @@
+import { takeLatest } from 'redux-saga/effects';
+import { CANCEL_ORDERS_EXECUTE } from '../constants';
+import { cancelOrdersSaga } from './cancelOrdersExecuteSaga';
+
+export function* rootCancelOrdersSaga() {
+    yield takeLatest(CANCEL_ORDERS_EXECUTE, cancelOrdersSaga);
+}
