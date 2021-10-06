b2735b0386ac3db84491aca375da7215f82d1aa9
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2021-10-06 18:35:50 +0530

    bug fixed, mulitple rendering on signUp

--- src/hooks/useKeyringInitalize.ts
+++ src/hooks/useKeyringInitalize.ts
@@ -4,6 +4,8 @@ import { useDispatch } from "react-redux";
 import { userListFetch } from "src/modules";
 import { polkadotWalletFetch } from "src/modules/user/polkadotWallet";
 import { Keyring } from '@polkadot/api'
+
+
 export const useKeyringInitalize = () => {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
@@ -14,19 +16,23 @@ export const useKeyringInitalize = () => {
     // console.log({ keyring_sr })
     try {
       keyring.loadAll({ ss58Format: 2, type: "ed25519" });
-      const p=keyring.addUri("sample split bamboo west visual approve brain fox arch impact relief smile")
+      const p = keyring.addUri("sample split bamboo west visual approve brain fox arch impact relief smile")
       console.log("the address we neeed", p.pair.address)
     } catch (e) {
       console.log("keyring loaded");
     }
-
     dispatch(userListFetch());
-    dispatch(polkadotWalletFetch())
     console.log("keyring crypto initalized");
     setLoading(false);
   };
+
+  useEffect(() => {
+    dispatch(polkadotWalletFetch())
+  }, [])
+
   useEffect(() => {
     loadKeyring();
   }, [dispatch]);
+  
   return { loading };
 };
