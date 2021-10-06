a070d36bbdb48939ad52f22d9859363ce5752493
charleslukes <charleslukes28@gmail.com>
2021-10-06 15:30:12 +0100

    add constants

---
+++ src/modules/user/OrdersTransactions/constants.ts
@@ -0,0 +1,6 @@
+export const PLACE_ORDERS_EXECUTE = 'place_orders/EXECUTE';
+export const CANCEL_ORDERS_EXECUTE = 'place_cancel/EXECUTE';
+
+export const CANCEL_SINGLE_ORDER = 'CANCEL_SINGLE_ORDER';
+export const PLACE_SINGLE_ORDER = 'PLACE_SINGLE_ORDER';
+
