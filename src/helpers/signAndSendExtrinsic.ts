e13cee07acc3c96aee70804c71f791f245d59be1
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2021-10-06 19:53:00 +0530

    updated error catching for extrinsics

--- src/helpers/signAndSendExtrinsic.ts
+++ src/helpers/signAndSendExtrinsic.ts
@@ -5,14 +5,19 @@ export const signAndSendExtrinsic = (
     injector: any,
     mainAddress: string) => {
     return new Promise((resolve, reject) => {
-        extrinsic.signAndSend(mainAddress, { signer: injector.signer }, ({ status }) => {
-            if (status.isInBlock) {
-                console.log(`Completed at block hash #${status.asInBlock.toString()}`);
-            } else {
-                console.log(`Current status: ${status.type}`);
+        extrinsic.signAndSend(mainAddress, { signer: injector.signer }, ({ events = [], status }) => {
+            if (status.isFinalized) {
+                events.forEach(({ event: { data, method, section }, phase }) => {
+                    console.log('', phase.toString(), `: ${section}.${method}`, data.toString());
+                });
+                console.log(`Transaction finalized at blockHash ${status.asFinalized}`);
+                resolve({success: true})
             }
-        }).catch((error: any) => {
-            console.log(':( transaction failed', error);
-        });
+            else {
+                console.log(`Current status is ${status}`);
+            }
+        }).catch(error => {
+            reject({sucess:false, error})
+        })
     })
 }
