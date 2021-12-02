2da94e9d24b74d551643aa0eceb71de8c1b86969
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2021-12-02 17:05:53 +0530

    average price shown incase of market order with mulitple trades

--- src/ui/organisms/OrderHistory/index.tsx
+++ src/ui/organisms/OrderHistory/index.tsx
@@ -76,11 +76,12 @@ export const OrderHistory = () => {
       {!fetching ? (
         <S.Content>
           {list?.map((item, i) => {
-            const { id, timestamp, price, amount, order_side, symbol, order_type } = item;
+            const { id, timestamp, price, amount, average, order_side, symbol, order_type } =
+              item;
             const priceFixed = currentMarket ? currentMarket.price_precision : 0;
             const amountFixed = currentMarket ? currentMarket.amount_precision : 0;
             const orderSide = order_side === "Sell";
-
+            const orderPrice = order_type === "Limit" ? price : average;
             const date = localeDate(new Date(timestamp), "fullDate");
             const CardComponent = width > 1130 ? OrderHistoryCard : OrderHistoryCardReponsive;
             return (
@@ -91,7 +92,7 @@ export const OrderHistory = () => {
                 quoteUnit={symbol[1]}
                 side={order_side.toUpperCase()}
                 isSell={orderSide}
-                price={Decimal.format(price, priceFixed, ",")}
+                price={Decimal.format(orderPrice, priceFixed, ",")}
                 amount={Decimal.format(amount, amountFixed, ",")}
                 total={"100"}
                 executed={"0.3"}
