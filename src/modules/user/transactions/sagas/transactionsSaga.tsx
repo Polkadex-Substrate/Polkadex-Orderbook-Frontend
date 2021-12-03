5381c2c3ca4d40c2571088495615cdc67b0cbf93
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2021-12-03 16:48:50 +0530

    replaced mock with URL for transactions

--- src/modules/user/transactions/sagas/transactionsSaga.tsx
+++ src/modules/user/transactions/sagas/transactionsSaga.tsx
@@ -18,27 +18,7 @@ export function* transactionsSaga() {
     const signature = yield call(() => signMessage(keyringPair, JSON.stringify(payload)));
     const data = formatPayload(signature, payload);
     // TODO: Replace url
-    // const res = yield call(() => API.post(ordersOption)("/fetch_user_trades", data));
-    const res = {
-      Fine: [
-        {
-          id: "1234",
-          txid: "0x68bfb29821c50ca35ef3762f887fd3211e4405aba1a94e448a4f218b850358f0",
-          timestamp: 1638431638238,
-          from: "5HGabetgTWWsoRJxdZP11ns2yi1oVaenXEBprfKHp7tpw7NH",
-          to: "5HGabemdto3ziZLeW1QQ8D4hevie1UWCk7T1mCR611Dc25WZ",
-          transaction_type: "Transfer",
-          amount: 24.0,
-          currency: "PDEX",
-          status: "Closed",
-          updated_timestamp: null,
-          fee: {
-            currency: "PDEX",
-            cost: 0.0,
-          },
-        },
-      ],
-    };
+    const res = yield call(() => API.post(ordersOption)("/fetch_transactions ", data));
     if (res.Fine) {
       yield put(transactionsData(res.Fine));
     } else {
