1f6c91232029dc4ee698bda4fb2c97a193de9558
Fredrik Simonsson <simson@polkadex.trade>
2022-01-28 10:12:18 +0900

    Remove openfinex http url

--- next.config.js
+++ next.config.js
@@ -17,13 +17,6 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   env: {
-    HOST_URL: process.env.NEXT_PUBLIC_HOST_URL || "http://openfinex.polkadex.trade:8000",
-    RANGER_HOST_URL:
-      process.env.NEXT_PUBLIC_RANGER_HOST_URL || "http://openfinex.polkadex.trade:8001",
-    PROXY_URL: process.env.NEXT_PUBLIC_PROXY_URL || "http://openfinex.polkadex.trade:15000",
-    PROXY_WS_URL:
-      process.env.NEXT_PUBLIC_PROXY_WS_URL || "http://openfinex.polkadex.trade:7070",
-    POLKADEX_WS: process.env.NEXT_PUBLIC_POLKADEX_WS || "wss://openfinex.polkadex.trade",
     POLKADEX_HOST_URL:
       process.env.NEXT_PUBLIC_POLKADEX_HOST_URL || "https://ramen-1.polkadex.trade:443/api",
     INFLUX_DB_URL:
