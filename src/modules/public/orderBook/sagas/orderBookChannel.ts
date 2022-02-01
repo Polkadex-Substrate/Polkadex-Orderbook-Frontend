513bba78bd3d344bb8dafa72154f1e3e64cf18de
Emmanuel <emmanuel.thomas237@gmail.com>
2022-02-01 16:25:07 +0530

    avoid collison in queue name

--- src/modules/public/orderBook/sagas/orderBookChannel.ts
+++ src/modules/public/orderBook/sagas/orderBookChannel.ts
@@ -1,24 +1,26 @@
 import { eventChannel } from "redux-saga";
 import { call, put, select, take } from "redux-saga/effects";
 import { u8aToString } from "@polkadot/util";
+import cryptoRandomString from "crypto-random-string";
 
 import { depthData, DepthState, orderBookData, OrderBookState } from "..";
 import { alertPush } from "../../alertHandler";
 import { RabbitmqChannelType, selectRabbitmqChannel } from "../../rabbitmqChannel";
 
+import { DEFAULT_RANDOM_STRING_LENGTH } from "@polkadex/web-constants";
+
 export function* orderBookChannelSaga() {
   try {
     const rabbitmqConn = yield select(selectRabbitmqChannel);
+    const queueName =
+      cryptoRandomString({ length: DEFAULT_RANDOM_STRING_LENGTH }) + "-orderbook-snapshot";
     if (rabbitmqConn) {
       const channel = yield call(() =>
-        fetchOrderBookChannel(
-          rabbitmqConn,
-          "one.orderbook-snapshot",
-          "BTC.USD.orderbook-snapshot"
-        )
+        fetchOrderBookChannel(rabbitmqConn, queueName, "*.*.orderbook-snapshot")
       );
       while (true) {
         const tradesMsg = yield take(channel);
+        console.log("orderbook channel", tradesMsg);
         const data: OrderBookState = JSON.parse(tradesMsg);
         const { asks, bids } = getDepthFromOrderbook(data);
         yield put(orderBookData(data));
