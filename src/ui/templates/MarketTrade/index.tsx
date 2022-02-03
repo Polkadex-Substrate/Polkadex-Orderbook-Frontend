94b057ac7ff835dd58dbdeaa5eb56bbd6f775889
Emmanuel <emmanuel.thomas237@gmail.com>
2022-02-04 02:20:23 +0530

    bug fix

--- src/ui/templates/MarketTrade/index.tsx
+++ src/ui/templates/MarketTrade/index.tsx
@@ -114,9 +114,9 @@ const getIsDecreasingArray = (recentTrades: PublicTrade[]): [boolean] => {
   for (let i = recentTrades.length - 1; i >= 0; i--) {
     if (i === recentTrades.length - 1) {
       res[i] = false;
-    } else if (recentTrades[i].price < recentTrades[i + 1].price) {
+    } else if (Number(recentTrades[i].price) < Number(recentTrades[i + 1].price)) {
       res[i] = true;
-    } else if (recentTrades[i].price === recentTrades[i + 1].price) {
+    } else if (Number(recentTrades[i].price) === Number(recentTrades[i + 1].price)) {
       res[i] = res[i + 1];
     } else res[i] = false;
   }
