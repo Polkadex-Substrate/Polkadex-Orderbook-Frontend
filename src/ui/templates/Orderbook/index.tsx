f96027215a1ff1d4dcc6755530aadd67b3ba0a1a
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-02-03 08:18:13 +0100

    fix: #68 Asks and Bids inverted

--- src/ui/templates/Orderbook/index.tsx
+++ src/ui/templates/Orderbook/index.tsx
@@ -73,8 +73,8 @@ export const Orderbook = () => {
       </S.Header>
       <S.Content>
         <OrderbookColumn
-          data={asks}
-          side="asks"
+          data={bids}
+          side="bids"
           maxVolume={maxVolume}
           handleSelectPrice={handleSelectPrice}
         />
@@ -92,8 +92,8 @@ export const Orderbook = () => {
           )}
         </S.Select>
         <OrderbookColumn
-          data={bids}
-          side="bids"
+          data={asks}
+          side="asks"
           maxVolume={maxVolume}
           handleSelectPrice={handleSelectPrice}
         />
