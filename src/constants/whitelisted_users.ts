d95cd6c04db8a0e044da6410e9b9fa00fddb8fe3
Emmanuel <emmanuel.thomas237@gmail.com>
2022-02-03 17:07:28 +0530

    eslint fix

--- src/constants/whitelisted_users.ts
+++ src/constants/whitelisted_users.ts
@@ -1,82 +1,89 @@
 export const whitelistedUsers = [
-    {
-        "mnemonic": "sadness patch develop chapter thumb lecture easily humor property tunnel cream industry",
-        "address": "5GEKzomm9Dcoo4YXs2CBbfFZA9nKwAVPFKKsQ2DmPrFbZvhx"
-    },
-    {
-        "mnemonic": "brief body slide little guilt retire switch prevent seed scare unlock bleak",
-        "address": "5GQyVPNNp2Dtdn7WuhCFiAyDufvxwyXKo4JEVkEjg9Cn6YYo"
-    },
-    {
-        "mnemonic": "faith eye cheese play shallow frozen remove annual know marriage almost taste",
-        "address": "5Ge7Bz4RS1jaezJMbGPe3SSYTWXPuFDUdsYH6PN5nrovCPSP"
-    },
-    {
-        "mnemonic": "expire lava panther peasant student winter harvest shuffle mention give depart assume",
-        "address": "5GNvwgnip9hE6nzkH7itYKTwK9SCCeENHhsvBCGNHS2EbPed"
-    },
-    {
-        "mnemonic": "pair buyer ridge parade muffin damp box feed spring nuclear hire point",
-        "address": "5EP1D1s5KQiwRQ4pVD6mRKB7VdGFoMYiucneBxKc3CHfMxKW"
-    },
-    {
-        "mnemonic": "gentle kingdom about film panic grain client buyer fiscal click knock slot",
-        "address": "5GRk23EUJx3CDJhkTgdGDxCCiPkgwiKei8qCgXzcznsiMevb"
-    },
-    {
-        "mnemonic": "lizard edge various wire flag throw mad scene slender bronze dumb clap",
-        "address": "5DUkprqW8b1Xbz34JQU2rekH71h4HWjmZ6jNUh45PZLpDGSF"
-    },
-    {
-        "mnemonic": "exhibit lend plug innocent gaze click raise note defy ship obey area",
-        "address": "5DXmY2VzymCKUcf7egdNRRTg4bGPhwu3rYUr6BeqmpP7BYAQ"
-    },
-    {
-        "mnemonic": "hollow viable kiwi bar clog fitness uphold cinnamon offer spike flavor board",
-        "address": "5HNkTJgxuYaRLN7sBYQjhepnkx9sSTvkoNPSJnEvcAf4q2e4"
-    },
-    {
-        "mnemonic": "oxygen pioneer tape wonder valve buzz left banana busy boss pair modify",
-        "address": "5C7nH9LJn2AbfUg9d8hGcFtdq5ojKDVhqB2TAUkcNQfY3LFj"
-    },
-    {
-        "mnemonic": "divorce jar foil mention hazard use march again casual marine decrease noodle",
-        "address": "5FhkhX2epbBDVmXAiSQXEgNRbHfq9apbTpVAFMDpyvRW2H24"
-    },
-    {
-        "mnemonic": "arena junior wrist maze twin reduce endless clarify drastic sample gap winter",
-        "address": "5EXbcm6wS728byHivdJxpe74Ngbj1Q9mf3KfxHFnyPmNo1x1"
-    },
-    {
-        "mnemonic": "thrive model female salt cherry notice choose swallow accuse uphold humor adult",
-        "address": "5D2Ff84oYobnF4VSxv9WzNJwzRo9pNJ3atgmeCq3DajFwayJ"
-    },
-    {
-        "mnemonic": "arena moon athlete mushroom will multiply kitchen meat brain walk point hire",
-        "address": "5DS5DP3p1Ta6TC597qjZqvTiEcA7zijBh3Ko7eAf2aSNrLDc"
-    },
-    {
-        "mnemonic": "fancy symbol jump sail favorite knock special language program drill forget long",
-        "address": "5FA9U7JAT6buRdJPRyaKBMfRfKgfhRBCTgwZ9qsLkzuH3Yno"
-    },
-    {
-        "mnemonic": "curve nasty price cancel portion album fringe duck pyramid ticket stand leisure",
-        "address": "5DEybvYJaNhtyCzCFCRDhxobaeMUhVJDU6gYpzRhNJFGtUT6"
-    },
-    {
-        "mnemonic": "bargain hotel duck year amount panel buyer holiday impulse rather prefer mind",
-        "address": "5CZxvCBqGJ6U5wkGSo8o6dCGY51A5PNYW6Fqg2sS3ByZJEqR"
-    },
-    {
-        "mnemonic": "festival diary flag come diagram reopen rude unveil twenty furnace forum pretty",
-        "address": "5H3gMxasp2fxx8wHKUrRL1YCydsZxANrMQpDXN5AQQtxmJCw"
-    },
-    {
-        "mnemonic": "grace erupt devote notice butter cheap toast comfort pig fork convince stuff",
-        "address": "5DcNHjFnJsmtjqnpCPprTSaqMpN2xPa6tanL2hF3tHxtswce"
-    },
-    {
-        "mnemonic": "faint scissors trick boss nuclear auto venture present twist female curtain wide",
-        "address": "5DtSmEXkdifN5znehhYSqP5T6PGu519hFwwtQ9pbdBgEJckF"
-    }
-]
+  {
+    mnemonic:
+      "sadness patch develop chapter thumb lecture easily humor property tunnel cream industry",
+    address: "5GEKzomm9Dcoo4YXs2CBbfFZA9nKwAVPFKKsQ2DmPrFbZvhx",
+  },
+  {
+    mnemonic: "brief body slide little guilt retire switch prevent seed scare unlock bleak",
+    address: "5GQyVPNNp2Dtdn7WuhCFiAyDufvxwyXKo4JEVkEjg9Cn6YYo",
+  },
+  {
+    mnemonic: "faith eye cheese play shallow frozen remove annual know marriage almost taste",
+    address: "5Ge7Bz4RS1jaezJMbGPe3SSYTWXPuFDUdsYH6PN5nrovCPSP",
+  },
+  {
+    mnemonic:
+      "expire lava panther peasant student winter harvest shuffle mention give depart assume",
+    address: "5GNvwgnip9hE6nzkH7itYKTwK9SCCeENHhsvBCGNHS2EbPed",
+  },
+  {
+    mnemonic: "pair buyer ridge parade muffin damp box feed spring nuclear hire point",
+    address: "5EP1D1s5KQiwRQ4pVD6mRKB7VdGFoMYiucneBxKc3CHfMxKW",
+  },
+  {
+    mnemonic: "gentle kingdom about film panic grain client buyer fiscal click knock slot",
+    address: "5GRk23EUJx3CDJhkTgdGDxCCiPkgwiKei8qCgXzcznsiMevb",
+  },
+  {
+    mnemonic: "lizard edge various wire flag throw mad scene slender bronze dumb clap",
+    address: "5DUkprqW8b1Xbz34JQU2rekH71h4HWjmZ6jNUh45PZLpDGSF",
+  },
+  {
+    mnemonic: "exhibit lend plug innocent gaze click raise note defy ship obey area",
+    address: "5DXmY2VzymCKUcf7egdNRRTg4bGPhwu3rYUr6BeqmpP7BYAQ",
+  },
+  {
+    mnemonic: "hollow viable kiwi bar clog fitness uphold cinnamon offer spike flavor board",
+    address: "5HNkTJgxuYaRLN7sBYQjhepnkx9sSTvkoNPSJnEvcAf4q2e4",
+  },
+  {
+    mnemonic: "oxygen pioneer tape wonder valve buzz left banana busy boss pair modify",
+    address: "5C7nH9LJn2AbfUg9d8hGcFtdq5ojKDVhqB2TAUkcNQfY3LFj",
+  },
+  {
+    mnemonic: "divorce jar foil mention hazard use march again casual marine decrease noodle",
+    address: "5FhkhX2epbBDVmXAiSQXEgNRbHfq9apbTpVAFMDpyvRW2H24",
+  },
+  {
+    mnemonic: "arena junior wrist maze twin reduce endless clarify drastic sample gap winter",
+    address: "5EXbcm6wS728byHivdJxpe74Ngbj1Q9mf3KfxHFnyPmNo1x1",
+  },
+  {
+    mnemonic:
+      "thrive model female salt cherry notice choose swallow accuse uphold humor adult",
+    address: "5D2Ff84oYobnF4VSxv9WzNJwzRo9pNJ3atgmeCq3DajFwayJ",
+  },
+  {
+    mnemonic: "arena moon athlete mushroom will multiply kitchen meat brain walk point hire",
+    address: "5DS5DP3p1Ta6TC597qjZqvTiEcA7zijBh3Ko7eAf2aSNrLDc",
+  },
+  {
+    mnemonic:
+      "fancy symbol jump sail favorite knock special language program drill forget long",
+    address: "5FA9U7JAT6buRdJPRyaKBMfRfKgfhRBCTgwZ9qsLkzuH3Yno",
+  },
+  {
+    mnemonic:
+      "curve nasty price cancel portion album fringe duck pyramid ticket stand leisure",
+    address: "5DEybvYJaNhtyCzCFCRDhxobaeMUhVJDU6gYpzRhNJFGtUT6",
+  },
+  {
+    mnemonic: "bargain hotel duck year amount panel buyer holiday impulse rather prefer mind",
+    address: "5CZxvCBqGJ6U5wkGSo8o6dCGY51A5PNYW6Fqg2sS3ByZJEqR",
+  },
+  {
+    mnemonic:
+      "festival diary flag come diagram reopen rude unveil twenty furnace forum pretty",
+    address: "5H3gMxasp2fxx8wHKUrRL1YCydsZxANrMQpDXN5AQQtxmJCw",
+  },
+  {
+    mnemonic: "grace erupt devote notice butter cheap toast comfort pig fork convince stuff",
+    address: "5DcNHjFnJsmtjqnpCPprTSaqMpN2xPa6tanL2hF3tHxtswce",
+  },
+  {
+    mnemonic:
+      "faint scissors trick boss nuclear auto venture present twist female curtain wide",
+    address: "5DtSmEXkdifN5znehhYSqP5T6PGu519hFwwtQ9pbdBgEJckF",
+  },
+];
