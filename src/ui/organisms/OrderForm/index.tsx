c99d4386b0a463045ab3b37a95138eb003763f63
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-02-04 13:55:00 +0100

    update: Unavailable amount validation

--- src/ui/organisms/OrderForm/index.tsx
+++ src/ui/organisms/OrderForm/index.tsx
@@ -181,13 +181,23 @@ export const MarketType = ({
     ? Number(availableBaseAmount) <= 0
     : Number(availableQuoteAmount) <= 0;
 
+  const amountUnavailable = isSellSide
+    ? Number(availableBaseAmount) >= Number(state.amountSell)
+    : Number(availableQuoteAmount) >= Number(state.amountBuy);
+
   const isDisabled = useMemo(() => {
     if (isMarket) {
-      return isLoading || !amountInput || amountAvailable;
+      return isLoading || !amountInput || amountAvailable || !amountUnavailable;
     } else {
-      return isLoading || !amountInput || !Number(state.price) || amountAvailable;
+      return (
+        isLoading ||
+        !amountInput ||
+        !Number(state.price) ||
+        amountAvailable ||
+        !amountUnavailable
+      );
     }
-  }, [isLoading, amountInput, amountAvailable, isMarket, state]);
+  }, [isLoading, amountInput, amountAvailable, isMarket, state, amountUnavailable]);
 
   return (
     <form>
