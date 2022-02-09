0d19df045868e080a2c55ab2feb941c819d75c70
Emmanuel <emmanuel.thomas237@gmail.com>
2022-02-09 17:27:57 +0530

    removed soon message

--- src/ui/organisms/Toolbar/index.tsx
+++ src/ui/organisms/Toolbar/index.tsx
@@ -51,65 +51,63 @@ export const Toolbar = () => {
           <Markets />
         </Dropdown>
       </S.Container>
-      <AvailableMessage message="Soon">
-        <S.InformationWrapper>
+      <S.InformationWrapper>
+        <Information
+          label="Last price"
+          text={
+            currentMarket &&
+            `${Decimal.format(
+              Number(getTickerValue("last")),
+              currentMarket?.price_precision,
+              ","
+            )} ${bidUnit || ""}`
+          }
+        />
+        <Information
+          label="Price 24h"
+          color={isPositive ? "green" : "red"}
+          text={currentMarket && `${Number(getTickerValue("price_change_percent"))}`}
+        />
+        <Information
+          label="Volume 24h"
+          text={
+            currentMarket &&
+            `${Decimal.format(
+              Number(getTickerValue("volume")),
+              currentMarket?.price_precision,
+              ","
+            )} ${bidUnit || ""}`
+          }
+        />
+        <div>
           <Information
-            label="Last price"
+            label="24h high"
+            color="green"
+            orientation="horizontal"
             text={
               currentMarket &&
               `${Decimal.format(
-                Number(getTickerValue("last")),
+                Number(getTickerValue("high")),
                 currentMarket?.price_precision,
                 ","
               )} ${bidUnit || ""}`
             }
           />
           <Information
-            label="Price 24h"
-            color={isPositive ? "green" : "red"}
-            text={currentMarket && `${Number(getTickerValue("price_change_percent"))}`}
-          />
-          <Information
-            label="Volume 24h"
+            label="24h low"
+            color="red"
+            orientation="horizontal"
             text={
               currentMarket &&
               `${Decimal.format(
-                Number(getTickerValue("volume")),
+                Number(getTickerValue("low")),
                 currentMarket?.price_precision,
                 ","
               )} ${bidUnit || ""}`
             }
           />
-          <div>
-            <Information
-              label="24h high"
-              color="green"
-              orientation="horizontal"
-              text={
-                currentMarket &&
-                `${Decimal.format(
-                  Number(getTickerValue("high")),
-                  currentMarket?.price_precision,
-                  ","
-                )} ${bidUnit || ""}`
-              }
-            />
-            <Information
-              label="24h low"
-              color="red"
-              orientation="horizontal"
-              text={
-                currentMarket &&
-                `${Decimal.format(
-                  Number(getTickerValue("low")),
-                  currentMarket?.price_precision,
-                  ","
-                )} ${bidUnit || ""}`
-              }
-            />
-          </div>
-        </S.InformationWrapper>
-      </AvailableMessage>
+        </div>
+      </S.InformationWrapper>
     </S.Wrapper>
   );
 };
