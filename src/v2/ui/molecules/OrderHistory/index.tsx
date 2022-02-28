e2009c8ce69d452275dc24cf482a9381bb1d2a4e
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-02-28 20:07:04 -0300

    update: cancelOrder

--- src/v2/ui/molecules/OrderHistory/index.tsx
+++ src/v2/ui/molecules/OrderHistory/index.tsx
@@ -1,73 +1,88 @@
+import { useDispatch } from "react-redux";
+
 import * as S from "./styles";
 
 import { AvailableMessage, Icon } from "@polkadex/orderbook-ui/molecules";
 import { Decimal } from "@polkadex/orderbook-ui/atoms";
 import { getSymbolFromAssetId } from "@polkadex/orderbook/helpers/assetIdHelpers";
+import { orderCancelFetch } from "@polkadex/orderbook-modules";
 import * as T from "@orderbook/v2/ui/molecules/OrderHistoryTable/types";
 
-export const OrderHistory = ({ orders, priceFixed, amountFixed }: T.Props) => (
-  <>
-    {orders?.map((order, i) => {
-      const date = new Date(Number(order.timestamp)).toLocaleTimeString();
-      const isSell = order.order_side === "Sell";
-      const isLimit = order.order_type === "Limit";
-      const baseUnit = getSymbolFromAssetId(order.base_asset);
-      const quoteUnit = getSymbolFromAssetId(order.quote_asset);
-      const filled = Number(order.filled_qty).toFixed(5);
+export const OrderHistory = ({ orders, priceFixed, amountFixed }: T.Props) => {
+  const dispatch = useDispatch();
+  return (
+    <>
+      {orders?.map((order, i) => {
+        const date = new Date(Number(order.timestamp)).toLocaleTimeString();
+        const isSell = order.order_side === "Sell";
+        const isLimit = order.order_type === "Limit";
+        const baseUnit = getSymbolFromAssetId(order.base_asset);
+        const quoteUnit = getSymbolFromAssetId(order.quote_asset);
+        const filled = Number(order.filled_qty).toFixed(5);
 
-      return (
-        <S.Card key={i} isOpenOrder={order.status === "Open"}>
-          <S.CardWrapper>
-            <S.CardBox>
-              <S.CardInfoToken>
-                <Icon name={isSell ? "OrderSell" : "OrderBuy"} size="large" color="text" />
-                <S.Tag isSell={isSell}>{order.order_side} </S.Tag>
-              </S.CardInfoToken>
-              <div>
-                <span>
-                  {baseUnit}/{quoteUnit}
-                </span>
-                <p>{date}</p>
-              </div>
-            </S.CardBox>
-            <S.CardInfo>
-              <span>{order.order_type}</span>
-              <p>Type</p>
-            </S.CardInfo>
-            {isLimit && (
+        return (
+          <S.Card key={i} isOpenOrder={order.status === "Open"}>
+            <S.CardWrapper>
+              <S.CardBox>
+                <S.CardInfoToken>
+                  <Icon name={isSell ? "OrderSell" : "OrderBuy"} size="large" color="text" />
+                  <S.Tag isSell={isSell}>{order.order_side} </S.Tag>
+                </S.CardInfoToken>
+                <div>
+                  <span>
+                    {baseUnit}/{quoteUnit}
+                  </span>
+                  <p>{date}</p>
+                </div>
+              </S.CardBox>
               <S.CardInfo>
-                <span>{Decimal.format(order.price, priceFixed, ",")}</span>
-                <p>Limit</p>
+                <span>{order.order_type}</span>
+                <p>Type</p>
               </S.CardInfo>
+              {isLimit && (
+                <S.CardInfo>
+                  <span>{Decimal.format(order.price, priceFixed, ",")}</span>
+                  <p>Limit({quoteUnit})</p>
+                </S.CardInfo>
+              )}
+              <S.CardInfo>
+                <span>{Decimal.format(order.amount, amountFixed, ",")}</span>
+                <p>Amount({isLimit ? baseUnit : quoteUnit})</p>
+              </S.CardInfo>
+              <S.CardInfo>
+                <span>{filled}</span>
+                <p>Filled</p>
+              </S.CardInfo>
+              <S.CardInfo>
+                <span>
+                  {Decimal.format(
+                    Number(order.price) * Number(order.amount),
+                    amountFixed,
+                    ","
+                  )}
+                </span>
+                <p>Total</p>
+              </S.CardInfo>
+            </S.CardWrapper>
+            {order.status === "Open" && (
+              <S.CardActions>
+                <div>
+                  <AvailableMessage message="Soon">
+                    <ul>
+                      <S.Cancel
+                        onClick={() => {
+                          dispatch(orderCancelFetch({ order_id: order.order_id }));
+                        }}>
+                        Cancel
+                      </S.Cancel>
+                    </ul>
+                  </AvailableMessage>
+                </div>
+              </S.CardActions>
             )}
-            <S.CardInfo>
-              <span>{Decimal.format(order.amount, amountFixed, ",")}</span>
-              <p>Amount</p>
-            </S.CardInfo>
-            <S.CardInfo>
-              <span>{filled}</span>
-              <p>Filled</p>
-            </S.CardInfo>
-            <S.CardInfo>
-              <span>
-                {Decimal.format(Number(order.price) * Number(order.amount), amountFixed, ",")}
-              </span>
-              <p>Total</p>
-            </S.CardInfo>
-          </S.CardWrapper>
-          {order.status === "Open" && (
-            <S.CardActions>
-              <div>
-                <AvailableMessage message="Soon">
-                  <ul>
-                    <S.Cancel>Cancel</S.Cancel>
-                  </ul>
-                </AvailableMessage>
-              </div>
-            </S.CardActions>
-          )}
-        </S.Card>
-      );
-    })}
-  </>
-);
+          </S.Card>
+        );
+      })}
+    </>
+  );
+};
