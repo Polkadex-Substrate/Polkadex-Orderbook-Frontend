8271fef66fb425cf253780348f4e361ef5ad57c7
nuel77 <emmanuel.thomas237@gmail.com>
2022-04-28 01:30:48 +0530

    sign and send extriinsic update

--- src/helpers/signAndSendExtrinsic.ts
+++ src/helpers/signAndSendExtrinsic.ts
@@ -1,30 +1,77 @@
+// TODO: Fix types
+import { ApiPromise } from "@polkadot/api";
 import { SubmittableExtrinsic } from "@polkadot/api/types";
 
-export interface ExtrinsicResponse {
-  success: boolean;
-  error?: any;
+export interface ExtrinsicResult {
+  isSuccess: boolean;
+  message?: string;
+  eventMessages?: string[];
+  hash: string;
 }
 
-export const signAndSendExtrinsic = (
+export const signAndSendExtrinsic = async (
+  api: ApiPromise,
   extrinsic: SubmittableExtrinsic<"promise", any>,
   injector: any,
-  mainAddress: string
+  address: string
 ) => {
-  return new Promise<ExtrinsicResponse>((resolve, reject) => {
+  return new Promise<ExtrinsicResult>((resolve, reject) => {
     extrinsic
-      .signAndSend(mainAddress, { signer: injector.signer }, ({ events = [], status }) => {
-        if (status.isFinalized) {
-          events.forEach(({ event: { data, method, section }, phase }) => {
-            console.log("", phase.toString(), `: ${section}.${method}`, data.toString());
-          });
-          console.log(`Transaction finalized at blockHash ${status.asFinalized}`);
-          resolve({ success: true });
-        } else {
-          console.log(`Current status is ${status}`);
+      .signAndSend(
+        address,
+        { signer: injector.signer },
+        ({ status, events, dispatchError }) => {
+          // status would still be set, but in the case of error we can shortcut
+          // to just check it (so an error would indicate InBlock or Finalized)
+          if (dispatchError) {
+            if (dispatchError.isModule) {
+              // for module errors, we have the section indexed, lookup
+              const decoded = api.registry.findMetaError(dispatchError.asModule);
+              const { docs, name, section } = decoded;
+
+              const errMsg = `${section}.${name}: ${docs.join(" ")}`;
+              reject(new Error(errMsg));
+            } else {
+              // Other, CannotLookup, BadOrigin, no extra info
+              const errMsg = dispatchError.toString();
+              reject(new Error(errMsg));
+            }
+          } else if (status.isInBlock) {
+            handleExtrinsicErrors(events, api);
+            const eventMessages = events.map(({ phase, event: { data, method, section } }) => {
+              return `event:${phase} ${section} ${method}:: ${data}`;
+            });
+            resolve({ isSuccess: true, eventMessages, hash: extrinsic.hash.toHex() });
+          }
         }
-      })
-      .catch((error) => {
-        resolve({ success: false, error });
+      )
+      .catch((e) => {
+        reject(new Error(e));
       });
   });
+};
+export const handleExtrinsicErrors = (events, api) => {
+  events
+    // find/filter for failed events
+    .filter(({ event }) => api.events.system.ExtrinsicFailed.is(event))
+    // we know that data for system.ExtrinsicFailed is
+    // (DispatchError, DispatchInfo)
+    .forEach(
+      ({
+        event: {
+          data: [error],
+        },
+      }) => {
+        if (error.isModule) {
+          // for module errors, we have the section indexed, lookup
+          const decoded = api.registry.findMetaError(error.asModule);
+          const { docs, method, section } = decoded;
+
+          console.log(`log: ${section}.${method}: ${docs.join(" ")}`);
+        } else {
+          // Other, CannotLookup, BadOrigin, no extra info
+          console.log("log: ", error.toString());
+        }
+      }
+    );
 };
