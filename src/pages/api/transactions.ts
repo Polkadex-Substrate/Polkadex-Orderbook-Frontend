6e562b9a01bc9594a10552633b4081e522e11b7f
charleslukes <charleslukes28@gmail.com>
2022-05-06 13:13:32 +0100

    add transaction routes

---
+++ src/pages/api/transactions.ts
@@ -0,0 +1,17 @@
+import { serializeBigInt } from "@polkadex/orderbook/helpers/serializeBigInt";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../prisma";
+
+export default async function transactions(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const transactions = await prisma.transactions.findMany();
+    const updatedData = transactions.map((acc) => {
+      return serializeBigInt(acc);
+    });
+
+    res.status(200).json({data: updatedData });
+  } catch (error) {
+    res.status(500).json({data: error.message})
+  }
+
+}
