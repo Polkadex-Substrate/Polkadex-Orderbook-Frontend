b4d770cedfe5ae083467ec45be9b985b16b8a13b
charleslukes <charleslukes28@gmail.com>
2022-05-11 16:07:10 +0100

    update user transaction route

--- src/pages/api/user/transactions/[id].ts
+++ src/pages/api/user/transactions/[id].ts
@@ -5,26 +5,33 @@ import prisma from "../../../../../prisma";
 export default async function transactions(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { id } = req.query;
-    const transactions = await prisma.transactions.findMany({
-      where: {
-        main_account_id: +id,
-      },
-      select: {
-        txid: true,
-        base_asset_type: true,
-        quote_asset_type: true,
-        order_type: true,
-        status: true,
-        qty: true,
-        price: true,
-      },
-    });
-    const updatedData = transactions.map((acc) => {
-      return serializeBigInt(acc);
-    });
-
-    res.status(200).json({ data: updatedData });
+    res.status(200).json({ data: userTransactions(+id) });
   } catch (error) {
     res.status(500).json({ data: error.message });
   }
 }
+
+export const userTransactions = async (
+  accId: number,
+  selector: Record<string, boolean> | null = {
+    txid: true,
+    base_asset_type: true,
+    quote_asset_type: true,
+    order_type: true,
+    status: true,
+    qty: true,
+    price: true,
+  }
+) => {
+  const transactions = await prisma.transactions.findMany({
+    where: {
+      main_account_id: accId,
+    },
+    select: selector
+  });
+  const updatedData = transactions.map((acc) => {
+    return serializeBigInt(acc);
+  });
+
+  return updatedData;
+};
