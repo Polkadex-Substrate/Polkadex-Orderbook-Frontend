4810d30dbe0b22ca89acefc373a0ce4471235afc
nuel77 <emmanuel.thomas237@gmail.com>
2022-05-21 03:23:00 +0530

    hook for balance fetch

---
+++ src/hooks/useUserDataFetch.ts
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { selectAssetsFetchSuccess } from "../modules/public/assets";
+import { selectCurrentMarket } from "../modules/public/markets";
+import { balancesFetch, selectBalancesSuccess } from "../modules/user/balances";
+import { selectUserInfo } from "../modules/user/profile";
+
+import { useReduxSelector } from "./useReduxSelector";
+
+export const useUserDataFetch = () => {
+  const user = useReduxSelector(selectUserInfo);
+  const dispatch = useDispatch();
+  const currentMarket = useReduxSelector(selectCurrentMarket);
+  const isAsssetsFetched = useReduxSelector(selectAssetsFetchSuccess);
+
+  useEffect(() => {
+    if (user) {
+      if (!isAsssetsFetched) dispatch(balancesFetch());
+    }
+  }, [user, dispatch, isAsssetsFetched]);
+};
