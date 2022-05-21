01f4284ea3b28211d1e80e60eeceb5e05e9a4487
nuel77 <emmanuel.thomas237@gmail.com>
2022-05-22 03:28:57 +0530

    order component update

--- src/v2/ui/molecules/OrderHistoryTable/index.tsx
+++ src/v2/ui/molecules/OrderHistoryTable/index.tsx
@@ -3,17 +3,16 @@ import * as T from "./types";
 import { TableRow, TableCard } from "@orderbook/v2/ui/molecules";
 import { localeDate } from "@polkadex/web-helpers";
 import { Decimal } from "@polkadex/orderbook-ui/atoms";
-import { getSymbolFromAssetId } from "@polkadex/orderbook/helpers/assetIdHelpers";
 
 export const OrderHistoryTable = ({ orders, priceFixed, amountFixed }: T.Props) => (
   <TableRow
     isOpenOrder={!!orders?.find((order) => order.status === "Open")}
     header={["Pair", "Date", "Type", "Price", "Amount", "Filled", "Total"]}>
     {orders.map((order, i) => {
-      const date = localeDate(new Date(Number(order.timestamp)), "fullDate");
-      const isSell = order.order_side === "Sell";
-      const baseUnit = getSymbolFromAssetId(order.base_asset);
-      const quoteUnit = getSymbolFromAssetId(order.quote_asset);
+      const date = new Date(order.timestamp).toLocaleString();
+      const isSell = order.order_side === "Ask";
+      const baseUnit = order.base_asset_type;
+      const quoteUnit = order.quote_asset_type;
       return (
         <TableCard
           key={i}
@@ -22,13 +21,13 @@ export const OrderHistoryTable = ({ orders, priceFixed, amountFixed }: T.Props) 
           orderSide={order.order_side}
           baseUnit={baseUnit}
           quoteUnit={quoteUnit}
-          filledQuantity={Number(order.filled_qty)}
+          filledQuantity={Number("0")}
           data={[
             { value: date },
             { value: order.order_type },
             { value: Decimal.format(order.price, priceFixed, ",") },
-            { value: Decimal.format(order.amount, amountFixed, ",") },
-            { value: Decimal.format(order.filled_qty, amountFixed, ",") },
+            { value: Decimal.format(order.qty, amountFixed, ",") },
+            { value: Decimal.format("0", amountFixed, ",") },
             {
               value: amountFixed.toFixed(5),
             },
