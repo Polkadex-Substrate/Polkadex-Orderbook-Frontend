85ea8e3486cfaa5f9dbfac3cfefdd27d722141a1
nuel77 <emmanuel.thomas237@gmail.com>
2022-05-22 03:28:22 +0530

    eslint fix + bug fix to sql query

--- src/pages/api/user/transactions/[id].ts
+++ src/pages/api/user/transactions/[id].ts
@@ -1,22 +1,27 @@
-import { serializeBigInt } from "@polkadex/orderbook/helpers/serializeBigInt";
 import { NextApiRequest, NextApiResponse } from "next";
+
 import prisma from "../../../../../prisma";
+
+import { serializeBigInt } from "@polkadex/orderbook/helpers/serializeBigInt";
 
 export default async function transactions(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { id } = req.query;
-    res.status(200).json({ data: userTransactions(+id) });
+    const data = await userTransactions(BigInt(id as string));
+    res.status(200).json({ data });
   } catch (error) {
     res.status(500).json({ data: error.message });
   }
 }
 
 export const userTransactions = async (
-  accId: number,
+  accId: bigint,
   selector: Record<string, boolean> | null = {
     txid: true,
     base_asset_type: true,
     quote_asset_type: true,
+    order_side: true,
+    timestamp: true,
     order_type: true,
     status: true,
     qty: true,
@@ -27,8 +32,9 @@ export const userTransactions = async (
     where: {
       main_account_id: accId,
     },
-    select: selector
+    select: selector,
   });
+  console.log("user transactions", transactions);
   const updatedData = transactions.map((acc) => {
     return serializeBigInt(acc);
   });
