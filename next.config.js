3d217d074c161fe54c2fa26995d1e680304dd393
nuel77 <emmanuel.thomas237@gmail.com>
2022-06-01 01:12:00 +0530

    added env variable for db

--- next.config.js
+++ next.config.js
@@ -18,9 +18,10 @@ module.exports = {
   },
   env: {
     API_PATH: process.env.API_PATH || "http://localhost:3000/api",
-    INFLUX_DB_URL:
-      process.env.INFLUX_DB_URL || "https://ec2-3-101-117-26.us-west-1.polkadex.trade/api",
-    POLKADEX_FEATURE: process.env.POLKADEX_FEATURE || "signUp",
+    DATABASE_URL: process.env.DATABASE_URL,
+    INFLUX_DB_URL: process.env.INFLUX_DB_URL || "https://ramen-1.polkadex.trade/api",
+    API2_PATH: process.env.API2_PATH || "https://ramen-1.polkadex.trade/api/",
+    POLKADEX_FEATURE: process.env.POLKADEX_FEATURE,
     INFLUX_DB_TOKEN: process.env.INFLUX_DB_TOKEN,
     RMQ_KEY: process.env.RMQ_KEY,
     RMQ_USERNAME: process.env.RMQ_USERNAME,
