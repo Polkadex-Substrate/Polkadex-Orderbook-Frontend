c7cd52e6ae49139068061bb1cdb2b271bf2158a9
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-07-04 08:18:03 -0300

    update: cancel order removed

--- src/v3/ui/molecules/OpenOrderCard/index.tsx
+++ src/v3/ui/molecules/OpenOrderCard/index.tsx
@@ -1,8 +1,23 @@
+import moment from "moment";
+import { useDispatch } from "react-redux";
+
 import * as S from "./styles";
 
 import { Icon } from "@polkadex/orderbook-ui/molecules";
+import { orderCancelFetch } from "@polkadex/orderbook-modules";
 
-const OpenOrderCard = ({ isSell, orderType, baseUnit, quoteUnit, data = [] }) => {
+const OpenOrderCard = ({
+  isSell,
+  orderSide,
+  orderType,
+  orderId,
+  base,
+  quote,
+  baseUnit,
+  quoteUnit,
+  data = [],
+}) => {
+  const dispatch = useDispatch();
   return (
     <S.Tr>
       <S.Td>
@@ -47,6 +62,11 @@ const OpenOrderCard = ({ isSell, orderType, baseUnit, quoteUnit, data = [] }) =>
         <S.Tag>Actions</S.Tag>
 
         <S.ContainerActions>
+          <button
+            type="button"
+            onClick={() => dispatch(orderCancelFetch({ orderId, base, quote }))}>
+            Cancel Order
+          </button>
           <Icon name="Options" size="medium" background="none" />
         </S.ContainerActions>
       </S.Td>
