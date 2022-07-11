0f74986f06ffedb91a30b091afbc1341ad6dd996
nuel77 <emmanuel.thomas237@gmail.com>
2022-07-12 02:03:03 +0530

    imporvements to withdraw page

--- src/hooks/useOnChainBalance.ts
+++ src/hooks/useOnChainBalance.ts
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 import { selectRangerApi, selectRangerIsReady } from "../modules/public/ranger";
 import { selectUserInfo } from "../modules/user/profile";
 import { fetchOnChainBalance } from "../helpers/fetchOnChainBalance";
+import { selectUserBalance } from "../modules/user/balances";
 
 import { useReduxSelector } from "./useReduxSelector";
 
 export const useOnChainBalance = (assetId: string) => {
   const api = useReduxSelector(selectRangerApi);
+  const balances = useReduxSelector(selectUserBalance);
   const isApiConnectd = useReduxSelector(selectRangerIsReady);
   const user = useReduxSelector(selectUserInfo);
   const [balance, setBalance] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(true);
 
+  // only update if the user balance changes
   useEffect(() => {
     if (isApiConnectd && user.main_addr) {
       fetchOnChainBalance(api, assetId, user.main_addr)
@@ -25,6 +28,7 @@ export const useOnChainBalance = (assetId: string) => {
           setLoading(false);
         });
     }
-  }, [api, assetId, isApiConnectd, user.main_addr]);
+  }, [balances]);
+
   return { onChainBalance: balance, onChainBalanceLoading: loading };
 };
