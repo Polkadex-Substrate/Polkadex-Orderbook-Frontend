7d310351a5a21c784bccc868731674d77bdc9446
nuel77 <emmanuel.thomas237@gmail.com>
2022-07-19 20:01:35 +0530

    disabled withdraw page

--- src/v3/ui/templates/Wallet/index.tsx
+++ src/v3/ui/templates/Wallet/index.tsx
@@ -49,9 +49,7 @@ export const WalletTemplate = () => {
                     <TabHeader>
                       <S.Tab color="green">Deposit</S.Tab>
                     </TabHeader>
-                    <TabHeader>
-                      <S.Tab color="primary">Withdraw</S.Tab>
-                    </TabHeader>
+                    <TabHeader>{/* <S.Tab color="primary">Withdraw</S.Tab> */}</TabHeader>
                   </S.Header>
                 </S.HeaderContainer>
               </S.EstimateBalance>
