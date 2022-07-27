1e51a5380ccb90b26a7c54729ba37111b1410370
charleslukes <charleslukes28@gmail.com>
2022-07-27 20:39:25 +0100

    update headers config

--- next.config.js
+++ next.config.js
@@ -6,9 +6,26 @@ module.exports = {
         source: "/(.*)?",
         headers: [
           {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'",
+          },
+          {
             key: "X-Frame-Options",
             value: "DENY",
           },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000'
+          }
         ],
       },
     ];
