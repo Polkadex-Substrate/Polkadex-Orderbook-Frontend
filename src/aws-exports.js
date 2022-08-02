e80644559a93881c7c271cab76db6feb8af93ed9
nuel77 <emmanuel.thomas237@gmail.com>
2022-08-02 09:47:26 +0530

    add graphql

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "ap-south-1",
-    "aws_appsync_graphqlEndpoint": "https://x6sbwzrbzvbabpujfy2phgq6ka.appsync-api.ap-south-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://pdbbdqroynawbgejojs4fjgfiq.appsync-api.ap-south-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-south-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-qqapvjh2unchne3osdf462anj4"
+    "aws_appsync_apiKey": "da2-uyr6rr4omrfmvdjh55mgiub5na"
 };
 
 
