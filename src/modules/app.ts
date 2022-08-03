ebc91615e5abb0add8a2086dae2a31fb2f995036
nuel77 <emmanuel.thomas237@gmail.com>
2022-08-04 03:26:08 +0530

    cleanup reducers

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -10,13 +10,11 @@ import { marketsReducer } from "./public/markets";
 import { depthReducer, orderBookReducer } from "./public/orderBook";
 import { recentTradesReducer } from "./public/recentTrades";
 import { authReducer } from "./user/auth";
-import { historyReducer } from "./user/history";
 import { ordersReducer } from "./user/orders";
 import { ordersHistoryReducer } from "./user/ordersHistory";
 import { notificationReducer } from "./user/notificationHandler";
 import { profileReducer } from "./user/profile";
 import { polkadotWalletReducer } from "./user/proxyAccount";
-import { walletsReducer } from "./user/wallets";
 import { depositsReducer } from "./user/deposit";
 import { tradesReducer } from "./user/trades";
 import { withdrawsReducer } from "./user/withdraws";
@@ -24,7 +22,6 @@ import { balancesReducer } from "./user/balances";
 import { transactionsReducer } from "./user/transactions";
 import { extensionWalletReducer } from "./user/mainAccount";
 import { sessionReducer } from "./user/session";
-import { eventListenerReducer } from "./public/eventsListener";
 import { assetsReducer } from "./public/assets";
 
 export const publicReducer = combineReducers({
@@ -38,7 +35,6 @@ export const publicReducer = combineReducers({
   depth: depthReducer,
   recentTrades: recentTradesReducer,
   ranger: rangerReducer,
-  enclaveRpcClient: eventListenerReducer,
   assets: assetsReducer,
 });
 
@@ -46,12 +42,10 @@ export const userReducer = combineReducers({
   auth: authReducer,
   polkadotWallet: polkadotWalletReducer,
   extensionWallet: extensionWalletReducer,
-  history: historyReducer,
   balances: balancesReducer,
   orders: ordersReducer,
   ordersHistory: ordersHistoryReducer,
   profile: profileReducer,
-  wallets: walletsReducer,
   deposits: depositsReducer,
   trades: tradesReducer,
   transactions: transactionsReducer,
