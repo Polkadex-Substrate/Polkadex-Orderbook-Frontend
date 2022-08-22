1e1806926e64609339ccfd119d0deb6f31fc8cbb
nuel77 <emmanuel.thomas237@gmail.com>
2022-08-22 11:55:43 +0530

    add limit to open orders fetch

--- src/modules/user/ordersHistory/sagas/openOrderHistorySaga.ts
+++ src/modules/user/ordersHistory/sagas/openOrderHistorySaga.ts
@@ -50,7 +50,7 @@ export function* openOrdersHistorySaga(action: UserOpenOrdersHistoryFetch) {
 const fetchOpenOrders = async (proxy_acc: string): Promise<OrderCommon[]> => {
   const res: any = await API.graphql({
     query: queries.listOpenOrdersByMainAccount,
-    variables: { main_account: proxy_acc },
+    variables: { main_account: proxy_acc, limit: 1000 },
   });
   const ordersRaw: orderHistoryQueryResult[] = res.data.listOpenOrdersByMainAccount.items;
   const orders = ordersRaw.map((order) => ({
