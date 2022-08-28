ae0dfe97c8289edfb309ef5c654905554132e9eb
charleslukes <charleslukes28@gmail.com>
2022-08-29 00:32:38 +0100

    set password

--- src/modules/user/tradeAccount/sagas/registerTradeAccountSaga.ts
+++ src/modules/user/tradeAccount/sagas/registerTradeAccountSaga.ts
@@ -13,6 +13,7 @@ import { notificationPush } from "../../notificationHandler";
 import { selectRangerApi } from "@polkadex/orderbook/modules/public/ranger";
 import { sendError } from "@polkadex/orderbook/modules/public/errorHandler";
 import { ExtrinsicResult, signAndSendExtrinsic } from "@polkadex/web-helpers";
+import { setToStorage } from "@polkadex/orderbook/helpers/storage";
 
 let tradeAddress: string;
 export function* registerTradeAccountSaga(action: RegisterTradeAccountFetch) {
@@ -55,6 +56,7 @@ export function* registerTradeAccountSaga(action: RegisterTradeAccountFetch) {
           })
         );
         yield put(registerTradeAccountData());
+        yield call(setIsTradeAccountPassworded, tradeAddress, password ? true : false);
       } else {
         throw new Error(res.message);
       }
@@ -84,4 +86,8 @@ export const addProxyToAccount = async (
   const ext = api.tx.ocex.addProxyAccount(proxyAddress);
   const res = await signAndSendExtrinsic(api, ext, injector, mainAddress, true);
   return res;
+};
+
+export const setIsTradeAccountPassworded = (tradeAddress: string, isPassworded: boolean) => {
+  setToStorage(`trading_acc_${tradeAddress}`, isPassworded);
 };
