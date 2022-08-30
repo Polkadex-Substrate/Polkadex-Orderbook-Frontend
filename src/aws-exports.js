518fbf45a662710f670a8c44c6a985e99b68ca79
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2022-08-30 21:24:40 +0530

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://uvdpw5pmrrasnor37rjt5zeqom.appsync-api.ap-south-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-south-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-ijfymin235bzfbrrrmc5c5dvpq",
+    "aws_appsync_apiKey": "da2-heoxyojw5zbwdijcsa7y6s5quy",
     "Auth": {
         "identityPoolId": "ap-south-1:608ede20-3f94-470a-a5b6-464112ed55b0",
         "region": "ap-south-1",
