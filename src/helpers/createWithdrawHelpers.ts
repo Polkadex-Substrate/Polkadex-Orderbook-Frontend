41fe2a95f8bb6e046139b5c303534961011dbd05
nuel77 <emmanuel.thomas237@gmail.com>
2022-08-30 09:49:13 +0530

    change nonce to timestamp

--- src/helpers/createWithdrawHelpers.ts
+++ src/helpers/createWithdrawHelpers.ts
@@ -4,12 +4,12 @@ export const createWithdrawPayload = (
   api: ApiPromise,
   asset: Record<string, string | null>,
   amount: string | number,
-  nonce: number
+  timestamp: number
 ) => {
   const withdraw_paylod = api.createType("WithdrawPayload", {
     asset_id: asset,
     amount,
-    nonce: nonce,
+    timestamp,
   });
   return withdraw_paylod;
 };
