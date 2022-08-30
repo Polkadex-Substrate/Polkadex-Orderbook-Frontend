b8a835d166c12502954bc212325faf15492f63b4
nuel77 <emmanuel.thomas237@gmail.com>
2022-08-30 11:12:04 +0530

    refactor to helper

--- src/hooks/useHistory.ts
+++ src/hooks/useHistory.ts
@@ -1,6 +1,8 @@
 import { useState, useEffect, useMemo, ChangeEvent, useCallback } from "react";
 import { useDispatch } from "react-redux";
 
+import { groupWithdrawsBySnapShotIds } from "../helpers/groupWithdrawsBySnapshotIds";
+
 import { selectGetAsset } from "@polkadex/orderbook/modules/public/assets";
 import { useReduxSelector } from "@polkadex/orderbook-hooks";
 import {
@@ -47,7 +49,7 @@ export function useHistory() {
   const deposits = transactionHistory.filter((txn) => txn.txn_type === "DEPOSIT");
 
   const withdrawals = useMemo(
-    () => groupWithdrawsByEventIds(withdrawalsList),
+    () => groupWithdrawsBySnapShotIds(withdrawalsList),
     [withdrawalsList]
   );
 
@@ -71,46 +73,3 @@ export function useHistory() {
     handleClaimWithdraws,
   };
 }
-
-export type WithdrawGroup = {
-  id: number;
-  sid: number;
-  items: WithdrawGroupItem[];
-};
-
-export type WithdrawGroupItem = {
-  id: number;
-  asset: string;
-  date: string;
-  event_id: number;
-  amount: string;
-  status: string;
-};
-
-// use event_id from withdraw list as block and index as id for withdraw item and data
-const groupWithdrawsByEventIds = (withdrawalsList: Transaction[]): WithdrawGroup[] => {
-  const withdrawals: WithdrawGroup[] = [];
-  const sidsProcessed: Set<number> = new Set();
-
-  withdrawalsList.forEach((withdrawal, index) => {
-    const id = index;
-    const sid = Number(withdrawal.sid);
-    const items: WithdrawGroupItem[] = [];
-    if (sidsProcessed.has(sid)) return;
-    withdrawalsList.forEach((item) => {
-      if (Number(item.sid) === sid) {
-        items.push({
-          id,
-          event_id: item.event_id,
-          asset: item.asset,
-          date: new Date(item.time).toLocaleString(),
-          amount: item.amount,
-          status: item.status,
-        });
-      }
-    });
-    sidsProcessed.add(sid);
-    withdrawals.push({ id, sid: sid, items });
-  });
-  return withdrawals;
-};
