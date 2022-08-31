3a0547dfca44f9a86869690729c01781187f5b2d
nuel77 <emmanuel.thomas237@gmail.com>
2022-08-31 11:08:36 +0530

    add remove proxy saga

--- src/modules/user/tradeAccount/sagas/index.ts
+++ src/modules/user/tradeAccount/sagas/index.ts
@@ -4,14 +4,17 @@ import {
   SET_CURRENT_TRADE_ACCOUNT,
   USER_REGISTER_TRADE_ACCOUNT_FETCH,
   USER_TRADE_ACCOUNTS_FETCH,
+  USER_TRADE_ACCOUNT_REMOVE_FROM_CHAIN_DATA,
 } from "../constants";
 
 import { loadTradeAccountsSaga } from "./loadTradeAccountsSaga";
 import { registerTradeAccountSaga } from "./registerTradeAccountSaga";
+import { removeProxyAccountFromChain } from "./removeTradeAccountFromChain";
 import { setCurrentTradeAccountSaga } from "./setCurrentTradeAccountSaga";
 
 export function* rootTradeAccountsSaga() {
   yield takeLatest(USER_TRADE_ACCOUNTS_FETCH, loadTradeAccountsSaga);
   yield takeLeading(USER_REGISTER_TRADE_ACCOUNT_FETCH, registerTradeAccountSaga);
   yield takeLatest(SET_CURRENT_TRADE_ACCOUNT, setCurrentTradeAccountSaga);
+  yield takeLeading(USER_TRADE_ACCOUNT_REMOVE_FROM_CHAIN_DATA, removeProxyAccountFromChain);
 }
