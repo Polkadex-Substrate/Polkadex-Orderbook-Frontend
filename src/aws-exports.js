e0561d277e856c818838c4af80cc0c3a5ee22276
nuel77 <emmanuel.thomas237@gmail.com>
2022-09-02 21:50:03 +0530

    chaneg aws api key

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,8 @@ const awsmobile = {
     "aws_project_region": "ap-south-1",
     "aws_appsync_graphqlEndpoint": "https://b652nqgwcvh4fnd57bfyeosq5e.appsync-api.ap-south-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-south-1",
-    "aws_appsync_authenticationType": 'AWS_LAMBDA',
+    "aws_appsync_authenticationType": 'API_KEY',
+    "aws_appsync_apiKey": "da2-6uhp2pcvxffb7nhb4pqrqhvgji",
     "Auth": {
         "identityPoolId": "ap-south-1:cd2e3e33-8e37-46c0-9d5d-7628020f6c4c",
         "region": "ap-south-1",
