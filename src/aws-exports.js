56945ac1e97b4705e51ec3f57ddb351f0ca1cb5d
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2022-09-03 20:58:50 +0530

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,16 +3,16 @@
 
 const awsmobile = {
     "aws_project_region": "ap-south-1",
-    "aws_appsync_graphqlEndpoint": "https://b652nqgwcvh4fnd57bfyeosq5e.appsync-api.ap-south-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://ka74yrg5pbe73p3gj6ozi2d2za.appsync-api.ap-south-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-south-1",
     "aws_appsync_authenticationType": 'API_KEY',
     "aws_appsync_apiKey": "da2-6uhp2pcvxffb7nhb4pqrqhvgji",
     "Auth": {
-        "identityPoolId": "ap-south-1:cd2e3e33-8e37-46c0-9d5d-7628020f6c4c",
+        "identityPoolId": "ap-south-1:82e648f0-6ca2-4ad0-a0d8-97b273eaf87e",
         "region": "ap-south-1",
         "identityPoolRegion": "ap-south-1",
-        "userPoolId": "ap-south-1_PjjzlJrWS",
-        "userPoolWebClientId": "7g7opgi0q66qg3jrplq6bb62eh"
+        "userPoolId": "ap-south-1_ckDYOZLN4",
+        "userPoolWebClientId": "5s5341f1pks1r1dqg0pm10kmc4"
     }
 };
 
