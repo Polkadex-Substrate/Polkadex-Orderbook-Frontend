aaa1854d71135f6c34ae359cbe9c4229b3ba8204
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-09-20 13:07:38 -0300

    feat: marketOrder passcode component

--- src/ui/molecules/MarketOrderAction/index.tsx
+++ src/ui/molecules/MarketOrderAction/index.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
+import { useMemo, useState } from "react";
+import { useFormik } from "formik";
 
 import * as S from "./styles";
 
-import { Icon, ButtonStatus, Range, MarketInput } from "@polkadex/orderbook-ui/molecules";
+import {
+  Icon,
+  ButtonStatus,
+  Range,
+  LoadingSection,
+  MarketInput,
+  PassCode,
+} from "@polkadex/orderbook-ui/molecules";
 import { usePlaceOrder } from "@polkadex/orderbook/hooks";
+import { Icons } from "@polkadex/orderbook-ui/atoms";
 
 export const MarketOrderAction = ({ isSell = false, isLimit }) => {
   const {
@@ -23,90 +33,144 @@ export const MarketOrderAction = ({ isSell = false, isLimit }) => {
     hasUser,
     isSignedIn,
     isOrderExecuted,
+    showProtectedPassword,
   } = usePlaceOrder(isSell, isLimit);
   return (
     <S.WrapperOrder>
-      <S.ContainerWallet>
-        <Icon
-          name="Wallet"
-          background="primaryBackgroundOpacity"
-          size="extraLarge"
-          stroke="text"
-        />
-        <S.WrapperBalance>
-          <span>Available</span>
-          <S.Span>
-            {availableAmount} {isSell ? baseTicker : quoteTicker}
-          </S.Span>
-        </S.WrapperBalance>
-      </S.ContainerWallet>
-      <S.ContainerForm>
-        <form onSubmit={executeOrder}>
-          {isLimit && (
-            <MarketInput
-              label="Price"
-              icon="Price"
-              inputInfo={quoteTicker}
-              fullWidth={true}
-              type="text"
-              placeholder="0.000000000"
-              id="order-price"
-              value={price}
-              autoComplete="off"
-              onChange={(e) => changePrice(e.currentTarget.value)}
-              disabled={isOrderLoading}
+      {showProtectedPassword ? (
+        <ProtectPassword />
+      ) : (
+        <>
+          <S.ContainerWallet>
+            <Icon
+              name="Wallet"
+              background="primaryBackgroundOpacity"
+              size="extraLarge"
+              stroke="text"
             />
-          )}
-          <MarketInput
-            label="Amount"
-            icon="Amount"
-            inputInfo={isLimit ? baseTicker : isSell ? baseTicker : quoteTicker}
-            fullWidth={true}
-            type="text"
-            placeholder="0.000000000"
-            id="order-amount"
-            value={amount}
-            autoComplete="off"
-            onChange={(e) => changeAmount(e.currentTarget.value)}
-            disabled={isOrderLoading}
-          />
-          <S.RangeWrapper>
-            <Range values={rangeValue} setValues={updateRange} />
-          </S.RangeWrapper>
+            <S.WrapperBalance>
+              <span>Available</span>
+              <S.Span>
+                {availableAmount} {isSell ? baseTicker : quoteTicker}
+              </S.Span>
+            </S.WrapperBalance>
+          </S.ContainerWallet>
+          <S.ContainerForm>
+            <form onSubmit={executeOrder}>
+              {isLimit && (
+                <MarketInput
+                  label="Price"
+                  icon="Price"
+                  inputInfo={quoteTicker}
+                  fullWidth={true}
+                  type="text"
+                  placeholder="0.000000000"
+                  id="order-price"
+                  value={price}
+                  autoComplete="off"
+                  onChange={(e) => changePrice(e.currentTarget.value)}
+                  disabled={isOrderLoading}
+                />
+              )}
+              <MarketInput
+                label="Amount"
+                icon="Amount"
+                inputInfo={isLimit ? baseTicker : isSell ? baseTicker : quoteTicker}
+                fullWidth={true}
+                type="text"
+                placeholder="0.000000000"
+                id="order-amount"
+                value={amount}
+                autoComplete="off"
+                onChange={(e) => changeAmount(e.currentTarget.value)}
+                disabled={isOrderLoading}
+              />
+              <S.RangeWrapper>
+                <Range values={rangeValue} setValues={updateRange} />
+              </S.RangeWrapper>
 
-          {isLimit && (
-            <MarketInput
-              label="Total"
-              inputInfo={isLimit ? quoteTicker : isSell ? quoteTicker : baseTicker}
-              fullWidth={true}
-              type="text"
-              defaultValue={total}
-              placeholder={isLimit ? "Total" : "Estimated Amount"}
-              autoComplete="off"
-              disabled={isOrderLoading}
-              readOnly
-            />
-          )}
-          {hasUser ? (
-            <ButtonStatus
-              isSell={isSell}
-              heading={{
-                text: !isSignedIn ? "Sign In to Place Order" : `${orderSide} ${baseTicker}`,
-                loading: "Waiting",
-                success: "Order Created",
-              }}
-              isLoading={isOrderLoading}
-              isSuccess={isOrderExecuted}
-              type="submit"
-              disabled={!hasUser || !isSignedIn}
-            />
-          ) : (
-            <Link href="/accountManager">
-              <S.Connect>Connect Trading Account</S.Connect>
-            </Link>
-          )}
-        </form>
-      </S.ContainerForm>
+              {isLimit && (
+                <MarketInput
+                  label="Total"
+                  inputInfo={isLimit ? quoteTicker : isSell ? quoteTicker : baseTicker}
+                  fullWidth={true}
+                  type="text"
+                  defaultValue={total}
+                  placeholder={isLimit ? "Total" : "Estimated Amount"}
+                  autoComplete="off"
+                  disabled={isOrderLoading}
+                  readOnly
+                />
+              )}
+              {hasUser ? (
+                <ButtonStatus
+                  isSell={isSell}
+                  heading={{
+                    text: !isSignedIn
+                      ? "Sign In to Place Order"
+                      : `${orderSide} ${baseTicker}`,
+                    loading: "Waiting",
+                    success: "Order Created",
+                  }}
+                  isLoading={isOrderLoading}
+                  isSuccess={isOrderExecuted}
+                  type="submit"
+                  disabled={!hasUser || !isSignedIn}
+                />
+              ) : (
+                <Link href="/accountManager">
+                  <S.Connect>Connect Trading Account</S.Connect>
+                </Link>
+              )}
+            </form>
+          </S.ContainerForm>
+        </>
+      )}
     </S.WrapperOrder>
+  );
+};
+
+const ProtectPassword = () => {
+  const { values, setFieldValue, handleSubmit } = useFormik({
+    initialValues: {
+      showPassword: false,
+      password: "",
+    },
+    onSubmit: (values) => isValidSize && console.log(values.password),
+  });
+
+  const isLoading = false;
+  const error = "";
+
+  const isValidSize = useMemo(
+    () => values?.password?.toString().length === 5,
+    [values.password]
+  );
+
+  return (
+    <LoadingSection isActive={isLoading} color="transparent">
+      <form onChange={handleSubmit}>
+        <S.ProtectPassword>
+          <S.ProtectPasswordTitle>
+            <span>Input 5-digit trading password</span>
+            <S.Show
+              type="button"
+              onClick={() => setFieldValue("showPassword", !values.showPassword)}>
+              {values.showPassword ? <Icons.Hidden /> : <Icons.Show />}
+            </S.Show>
+          </S.ProtectPasswordTitle>
+          <S.ProtectPasswordContent>
+            <PassCode
+              numInputs={5}
+              onChange={(e) => setFieldValue("password", e)}
+              value={values.password}
+              name="password"
+              error={error.length && error}
+              type={values.showPassword ? "password" : "tel"}
+            />
+          </S.ProtectPasswordContent>
+        </S.ProtectPassword>
+      </form>
+    </LoadingSection>
   );
 };
