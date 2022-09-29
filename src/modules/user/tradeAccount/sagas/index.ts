df6a282d5049b6c74af3768b5cc1aa9783506682
nuel77 <emmanuel.thomas237@gmail.com>
2022-09-29 15:03:58 +0530

    add import account functionality

--- src/modules/user/tradeAccount/sagas/index.ts
+++ src/modules/user/tradeAccount/sagas/index.ts
@@ -5,12 +5,14 @@ import {
   USER_REGISTER_TRADE_ACCOUNT_FETCH,
   USER_TRADE_ACCOUNTS_FETCH,
   USER_TRADE_ACCOUNT_REMOVE_FROM_CHAIN_FETCH,
+  USER_TRADE_ACCOUNT_IMPORT_FETCH,
 } from "../constants";
 
 import { loadTradeAccountsSaga } from "./loadTradeAccountsSaga";
 import { registerTradeAccountSaga } from "./registerTradeAccountSaga";
 import { removeProxyAccountFromChainSaga } from "./removeTradeAccountFromChain";
 import { setCurrentTradeAccountSaga } from "./setCurrentTradeAccountSaga";
+import { importTradeAccountFetchSaga } from "./importTradeAccountSaga";
 
 export function* rootTradeAccountsSaga() {
   yield takeLatest(USER_TRADE_ACCOUNTS_FETCH, loadTradeAccountsSaga);
@@ -20,4 +22,5 @@ export function* rootTradeAccountsSaga() {
     USER_TRADE_ACCOUNT_REMOVE_FROM_CHAIN_FETCH,
     removeProxyAccountFromChainSaga
   );
+  yield takeLeading(USER_TRADE_ACCOUNT_IMPORT_FETCH, importTradeAccountFetchSaga);
 }
