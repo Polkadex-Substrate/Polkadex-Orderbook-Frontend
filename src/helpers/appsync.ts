0adecf4a88ec79e6445654cf7033e0af4af55ded
nuel77 <emmanuel.thomas237@gmail.com>
2022-10-06 13:21:58 +0530

    update query sender

--- src/helpers/appsync.ts
+++ src/helpers/appsync.ts
@@ -1,16 +1,28 @@
 import { API } from "aws-amplify";
+import { GRAPHQL_AUTH_MODE } from "@aws-amplify/auth";
 
 import { READ_ONLY_TOKEN } from "@polkadex/web-constants";
 
 export const sendQueryToAppSync = async (
   query: string,
   variables?: Record<string, any>,
-  token?: string
+  token?: string,
+  authMode: keyof typeof GRAPHQL_AUTH_MODE = "AWS_LAMBDA"
 ) => {
-  const res = await API.graphql({
-    query,
-    variables,
-    authToken: token ?? READ_ONLY_TOKEN,
-  });
+  let res: any;
+  if (authMode === "AWS_LAMBDA") {
+    res = await API.graphql({
+      query,
+      variables,
+      authToken: token ?? READ_ONLY_TOKEN,
+    });
+  }
+  if (authMode === "AMAZON_COGNITO_USER_POOLS") {
+    res = await API.graphql({
+      query,
+      variables,
+      authMode,
+    });
+  }
   return res;
 };
