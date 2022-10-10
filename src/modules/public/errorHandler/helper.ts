4985e44b8959ed85b45c2db5ada1ca6eb02a8925
charleslukes <charleslukes28@gmail.com>
2022-10-10 11:22:47 +0100

    add helper methopd

---
+++ src/modules/public/errorHandler/helper.ts
@@ -0,0 +1,12 @@
+const transFormErrorMessage = (message: string) => {
+  const value = message?.toLowerCase()
+  if(value.includes("ocex.proxylimitexceeded")){
+    return "You can't create more than three proxy accounts"
+  }
+
+  if(message.includes("ocex.mainaccountalreadyregistered")){
+    return "Account already registered"
+  }
+
+  return message;
+}
