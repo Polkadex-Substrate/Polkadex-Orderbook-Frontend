ed00e92ad0232f3e8bb4dc28930acb63261e49db
nuel77 <emmanuel.thomas237@gmail.com>
2022-10-17 09:49:45 +0530

    fix balance updat issue for place order

--- src/modules/user/balances/selectors.tsx
+++ src/modules/user/balances/selectors.tsx
@@ -1,8 +1,6 @@
 import { RootState } from "../../";
 
-import { Balance, BalanceBase } from ".";
-
-import { isAssetPDEX } from "@polkadex/orderbook/modules/public/assets";
+import { Balance } from ".";
 
 export const selectBalancesSuccess = (state: RootState): boolean =>
   state.user.balances.success;
@@ -14,11 +12,11 @@ export const selectBalancesLoading = (state: RootState): boolean =>
 
 export const selectUserBalance = (state: RootState): Balance[] => state.user.balances.balances;
 
-export const selectGetFreeProxyBalance =
+export const selectGetFreeTradeBalance =
   (state: RootState): ((assetId: string) => string) =>
   (assetId: string) => {
     const balance = state?.user?.balances?.balances?.find(
-      (balance) => balance?.asset_id?.toString() === assetId || isAssetPDEX(balance?.asset_id)
+      (balance) => balance?.asset_id?.toString() === assetId
     );
     if (!balance?.asset_id) return "0";
     return balance.free_balance;
