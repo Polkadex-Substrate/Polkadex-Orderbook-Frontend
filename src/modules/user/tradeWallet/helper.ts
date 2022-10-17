e8a46eda7f969509f26d0c665c8f6a6c6fbe88d0
Charles Chiakwa <charleslukes28@gmail.com>
2022-10-17 14:44:42 +0100

    Feat/account update (#394)
    * update
    
    * update upload feat
    
    * add file saver
    
    * update actions
    
    * save trade account to local store
    
    * add trade account to redux store
    
    * save trade acc
    
    * update template
    
    Co-authored-by: Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>

---
+++ src/modules/user/tradeWallet/helper.ts
@@ -0,0 +1,10 @@
+import { TradeAccount } from "../../types";
+
+// This is needed as the selector one can not be used inside a function.
+export const getTradeAccount = (addr: string, tradeAccounts: TradeAccount[]) => {
+  const acc = tradeAccounts.find(
+    (tradeAcc) => tradeAcc.address?.toLowerCase() === addr?.toLowerCase()
+  );
+
+  return acc;
+}
