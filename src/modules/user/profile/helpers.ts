e49131ff46c44e26e30f11fe708ad17645f01d9d
charleslukes <charleslukes28@gmail.com>
2022-10-17 10:07:56 +0100

    save trade account

--- src/modules/user/profile/helpers.ts
+++ src/modules/user/profile/helpers.ts
@@ -14,5 +14,35 @@ export const transformAddress = (address: string) => {
   const firstPart = address.substring(0, 12);
   const lastPart = address.substring(37, address.length);
 
-  return `${firstPart}...${lastPart}`
-}
+  return `${firstPart}...${lastPart}`;
+};
+
+export const saveTradeAccountsToLocalCache = (account: {
+  tradeAddress: string;
+  mainAddress: string;
+}) => {
+  const id = "userTradingAccounts";
+  const userTradingAccounts = window.localStorage.getItem(id);
+  if (userTradingAccounts) {
+    const userTradingAccJson: Array<{ tradeAddress: string; mainAddress: string; isActive: boolean }> =
+      JSON.parse(userTradingAccounts);
+    let isTradingAccountPresent = null;
+    userTradingAccJson.forEach((acc) => {
+      if (acc.tradeAddress.toLowerCase() === account.tradeAddress.toLowerCase()) {
+        isTradingAccountPresent = true;
+        acc.isActive = true;
+      } else {
+        acc.isActive = false;
+      }
+    });
+    if (!isTradingAccountPresent) {
+      userTradingAccJson.unshift({ ...account, isActive: true });
+    }
+    userTradingAccJson.splice(3);
+  }
+
+  window.localStorage.setItem(
+    id,
+    userTradingAccounts ? JSON.stringify(userTradingAccounts) : "[]"
+  );
+};
