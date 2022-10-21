e9b85c992d7681427fb10db97101ab1b0e29bfa7
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-10-21 15:00:50 -0300

    chore: 🔥 remove unused components

--- src/file-to-delete/v2/ui/molecules/Range/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from "./styles";
-
-const Range = () => (
-  <S.Wrapper>
-    <S.Input type="range" min="0" max="100" />
-  </S.Wrapper>
-);
-
-export default Range;
--- src/file-to-delete/v2/ui/molecules/Status/types.ts
+++
@@ -1,4 +0,0 @@
-export type Props = {
-  isActive: boolean;
-  isLoading: boolean;
-};
