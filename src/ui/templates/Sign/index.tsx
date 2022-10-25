bb8c39a7d5b0bc37b7ab72fd0846d2dd94b6a327
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-10-25 13:54:09 -0300

    update: fix terms and privacy links

--- src/ui/templates/Sign/index.tsx
+++ src/ui/templates/Sign/index.tsx
@@ -120,7 +120,7 @@ export const SignTemplate = () => {
                       <span>
                         By clicking the submit button below,I hereby agree with Polkadex{" "}
                         <a
-                          href="https://github.com/Polkadex-Substrate/Docs/blob/master/Polkadex_Privacy_Policy.pdf"
+                          href="https://github.com/Polkadex-Substrate/Docs/blob/master/Polkadex_Orderbook_Terms_of_Use.pdf"
                           target="_blank"
                           rel="noreferrer">
                           Terms of Service
