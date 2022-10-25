0d99f5c24683ef5b0577eede79f95ba792b3baa2
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-10-25 12:35:33 -0300

    update: persist market

--- src/pages/trading/index.tsx
+++ src/pages/trading/index.tsx
@@ -1,14 +1,19 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 import { defaultConfig } from "@polkadex/orderbook-config";
+import { LOCAL_STORAGE_ID } from "@polkadex/web-constants";
 
 function Home() {
   const router = useRouter();
+  const persistedMarket = useMemo(
+    () => process.browser && window.localStorage.getItem(LOCAL_STORAGE_ID.DEFAULT_MARKET),
+    []
+  );
 
   useEffect(() => {
-    router.push("/trading/" + defaultConfig.landingPageMarket);
-  }, [router]);
+    router.push(`/trading/${defaultConfig.landingPageMarket}`);
+  }, [router, persistedMarket]);
 
   return <div />;
 }
