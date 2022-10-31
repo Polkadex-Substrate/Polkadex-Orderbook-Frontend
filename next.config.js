ea753014eed4162e1b87f08d4636038fc1183949
minikas <kas_pintxuki@outlook.com>
2022-10-31 10:58:49 -0300

    update: force redeploy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require("next-compose-plugins");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === "false",
 });
 
 module.exports = withPlugins([
