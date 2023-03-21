5d6f8fef5b75af38680ee06f3afabfea872984a9
ap211unitech <porwal.1@iitj.ac.in>
2023-03-21 15:06:09 +0530

    Reducer Created for Orders Provider

--- src/providers/user/orders/reducer.ts
+++ src/providers/user/orders/reducer.ts
@@ -1,4 +1,5 @@
 import { OrdersState } from "./types";
+import { OrdersAction } from "./actions";
 import {
   ORDER_EXECUTE_DATA,
   ORDER_EXECUTE_ERROR,
@@ -28,8 +29,76 @@ export const initialState: OrdersState = {
   orderType: "",
 };
 
-export const ordersReducer = (state: OrdersState, action: any) => {
+export const ordersReducer = (state: OrdersState, action: OrdersAction) => {
   switch (action.type) {
+    case ORDER_EXECUTE_FETCH:
+      return {
+        ...state,
+        execute: { ...state.execute, isLoading: true, isError: false },
+      };
+    case ORDER_EXECUTE_DATA:
+      return {
+        ...state,
+        execute: { ...state.execute, isLoading: false, isError: false, isSuccess: true },
+      };
+    case ORDER_EXECUTE_DATA_DELETE:
+      return {
+        ...state,
+        execute: { ...state.execute, isLoading: false, isSuccess: false },
+      };
+    case ORDER_EXECUTE_ERROR:
+      return {
+        ...state,
+        execute: {
+          ...state.execute,
+          isLoading: false,
+          isError: true,
+          message: action.error.message,
+        },
+      };
+    case ORDER_CANCEL_FETCH:
+      return {
+        ...state,
+        cancel: { ...state.cancel, isLoading: true, isError: false },
+      };
+    case ORDER_CANCEL_DATA: {
+      return {
+        ...state,
+        cancel: { ...state.cancel, isLoading: false, isError: false, isSuccess: true },
+      };
+    }
+    case ORDER_CANCEL_DATA_DELETE: {
+      return {
+        ...state,
+        cancel: { ...state.cancel, isSuccess: false },
+      };
+    }
+    case ORDER_CANCEL_ERROR: {
+      return {
+        ...state,
+        cancel: {
+          ...state.cancel,
+          isLoading: false,
+          isError: true,
+          message: action.error.message,
+        },
+      };
+    }
+    case ORDERS_SET_CURRENT_PRICE:
+      return {
+        ...state,
+        currentPrice: action.payload,
+      };
+    case ORDERS_SET_AMOUNT:
+      return {
+        ...state,
+        amount: action.payload,
+      };
+    case ORDERS_SET_ORDER_TYPE:
+      return {
+        ...state,
+        orderType: action.payload,
+      };
     default:
       return state;
   }
