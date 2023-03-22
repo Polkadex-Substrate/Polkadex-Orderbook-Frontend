af8cbf8042cb3538b2af92d3496422fac9c6d0d4
ap211unitech <porwal.1@iitj.ac.in>
2023-03-22 13:54:24 +0530

    Reducer Created for Orderbook

--- src/providers/user/orderBook/reducer.ts
+++ src/providers/user/orderBook/reducer.ts
@@ -1,3 +1,7 @@
+import _ from "lodash";
+import { defaultConfig } from "@polkadex/orderbook-config";
+import { deleteFromBook, replaceOrAddToBook, sliceArray } from "@polkadex/web-helpers";
+
 import {
   DEPTH_DATA,
   DEPTH_ERROR,
@@ -8,14 +12,98 @@ import {
   ORDER_BOOK_FETCH,
 } from "./constants";
 import { OrderBookState } from "./types";
+import { OrderBookActions, DepthActions } from "./actions";
+
+const { orderBookSideLimit } = defaultConfig;
 
 export const initialOrderBook: OrderBookState = {
   orderbook: { asks: [], bids: [], loading: false },
   depth: { asks: [], bids: [], loading: false },
 };
 
-export const orderBookReducer = (state: OrderBookState, action: any) => {
+export const orderBookReducer = (
+  state: OrderBookState,
+  action: OrderBookActions | DepthActions
+) => {
   switch (action.type) {
+    case ORDER_BOOK_FETCH:
+      return {
+        ...state,
+        orderbook: {
+          ...state.orderbook,
+          loading: true,
+          error: undefined,
+        },
+      };
+    case ORDER_BOOK_DATA: {
+      const { asks, bids } = action.payload;
+
+      return {
+        ...state,
+        orderbook: {
+          asks: sliceArray(asks, orderBookSideLimit),
+          bids: sliceArray(bids, orderBookSideLimit),
+          loading: false,
+          error: undefined,
+        },
+      };
+    }
+    case ORDER_BOOK_ERROR:
+      return {
+        ...state,
+        orderbook: {
+          ...state.orderbook,
+          loading: false,
+          error: action.error,
+        },
+      };
+    case DEPTH_FETCH:
+      return {
+        ...state,
+        depth: {
+          loading: true,
+          error: undefined,
+        },
+      };
+    case DEPTH_DATA: {
+      const { asks, bids } = action.payload;
+
+      return {
+        ...state,
+        depth: {
+          asks: sliceArray(asks, orderBookSideLimit),
+          bids: sliceArray(bids, orderBookSideLimit),
+          loading: false,
+          error: undefined,
+        },
+      };
+    }
+    case DEPTH_ERROR:
+      return {
+        ...state,
+        depth: {
+          ...state.depth,
+          loading: false,
+          error: action.error,
+        },
+      };
+    case DEPTH_DATA_INCREMENT: {
+      let book = { ask: [...state.depth.asks], bid: [...state.depth.bids] };
+      const incrementalData = action.payload;
+      incrementalData.forEach((item) => {
+        if (Number(item.qty) === 0) {
+          book = deleteFromBook(book, item.price, item.side.toLowerCase());
+        } else book = replaceOrAddToBook(book, item.price, item.qty, item.side.toLowerCase());
+      });
+      return {
+        ...state,
+        depth: {
+          ...state.depth,
+          asks: _.cloneDeep(book.ask),
+          bids: _.cloneDeep(book.bid),
+        },
+      };
+    }
     default:
       return state;
   }
