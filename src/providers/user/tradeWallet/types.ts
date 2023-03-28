ae80e53f3fa2aa892cc9888cc2157f9ea732e3b3
ap211unitech <porwal.1@iitj.ac.in>
2023-03-28 19:34:06 +0530

    Changes in types.ts

--- src/providers/user/tradeWallet/types.ts
+++ src/providers/user/tradeWallet/types.ts
@@ -1,5 +1,5 @@
 import { FC, PropsWithChildren } from "react";
-import { CommonActionState, TradeAccount } from "../../types";
+import { TradeAccount } from "../../types";
 import { IUserTradeAccount } from "@polkadex/orderbook/hooks/types";
 
 export interface TradeWalletState {
