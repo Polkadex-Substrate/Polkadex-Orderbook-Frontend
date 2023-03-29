e73149bdbbd3d0337f0593d8e29ec68dc70f4370
ap211unitech <porwal.1@iitj.ac.in>
2023-03-29 15:22:48 +0530

    Ranger Provider Boilerplate

---
+++ src/providers/public/ranger/constants.ts
@@ -0,0 +1,6 @@
+export const RANGER_CONNECT_FETCH = "ranger/CONNECT_FETCH";
+export const RANGER_CONNECT_DATA = "ranger/CONNECT_DATA";
+export const RANGER_CONNECT_ERROR = "ranger/CONNECT_ERROR";
+export const RANGER_DISCONNECT_FETCH = "ranger/DISCONNECT_FETCH";
+export const RANGER_DISCONNECT_DATA = "ranger/DISCONNECT_DATA";
+export const RANGER_NO_EXTENSION = "ranger/NO_EXTENSION";
---
+++ src/providers/public/ranger/reducer.ts
@@ -0,0 +1,51 @@
+import { RangerAction } from "./actions";
+import {
+  RANGER_CONNECT_DATA,
+  RANGER_CONNECT_ERROR,
+  RANGER_CONNECT_FETCH,
+  RANGER_DISCONNECT_DATA,
+  RANGER_NO_EXTENSION,
+} from "./constants";
+
+import { RangerState } from "./types";
+
+export const initialRangerState: RangerState = {
+  connected: false,
+  connecting: false,
+  hasExtension: true,
+};
+
+export const rangerReducer = (
+  state = initialRangerState,
+  action: RangerAction
+): RangerState => {
+  switch (action.type) {
+    case RANGER_CONNECT_FETCH:
+      return {
+        ...state,
+        connected: false,
+        connecting: true,
+        timestamp: Math.floor(Date.now() / 1000),
+      };
+
+    case RANGER_CONNECT_DATA:
+      return {
+        ...state,
+        connected: true,
+        connecting: false,
+        api: action.payload,
+      };
+
+    case RANGER_CONNECT_ERROR:
+    case RANGER_DISCONNECT_DATA:
+      return {
+        ...state,
+        connected: false,
+        connecting: false,
+        hasExtension: false,
+      };
+    default:
+  }
+
+  return state;
+};
