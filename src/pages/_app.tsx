008754a338b097ed62033f1416bd5eaf2031470e
ap211unitech <porwal.1@iitj.ac.in>
2023-04-03 15:39:05 +0530

    ExtensionWalletProvider integrated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,6 +22,7 @@ import { OrderBookProvider } from "@polkadex/orderbook/providers/public/orderBoo
 import { AuthProvider, useAuth } from "@polkadex/orderbook/providers/user/auth";
 import { ProfileProvider, useProfile } from "@polkadex/orderbook/providers/user/profile";
 import { NativeApiProvider } from "@polkadex/orderbook/providers/public/nativeApi";
+import { ExtensionWalletProvider } from '@polkadex/orderbook/providers/user/extensionWallet'
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -71,22 +72,24 @@ function App({ Component, pageProps }: AppProps) {
           <AssetsProvider onError={(v) => toast.error(v)} onNotification={(v) => toast.info(v)}>
             <NativeApiProvider onError={(v) => toast.error(v)} onNotification={(v) => toast.info(v)}>
               <OrderBookProvider onError={(v) => toast.error(v)} onNotification={(v) => toast.info(v)}>
-                <OverlayProvider>
-                  <ThemeProvider
-                    theme={color === "light" ? defaultThemes.light : defaultThemes.dark}>
-                    {defaultConfig.maintenanceMode ? (
-                      <Maintenance />
-                    ) : (
-                      <QueryClientProvider client={queryClient}>
-                        <ThemeWrapper>
-                          <Component {...pageProps} />
-                        </ThemeWrapper>
-                      </QueryClientProvider>
-                    )}
+                <ExtensionWalletProvider onError={(v) => toast.error(v)} onNotification={(v) => toast.info(v)}>
+                  <OverlayProvider>
+                    <ThemeProvider
+                      theme={color === "light" ? defaultThemes.light : defaultThemes.dark}>
+                      {defaultConfig.maintenanceMode ? (
+                        <Maintenance />
+                      ) : (
+                        <QueryClientProvider client={queryClient}>
+                          <ThemeWrapper>
+                            <Component {...pageProps} />
+                          </ThemeWrapper>
+                        </QueryClientProvider>
+                      )}
 
-                    <GlobalStyles />
-                  </ThemeProvider>
-                </OverlayProvider>
+                      <GlobalStyles />
+                    </ThemeProvider>
+                  </OverlayProvider>
+                </ExtensionWalletProvider>
               </OrderBookProvider>
             </NativeApiProvider>
           </AssetsProvider>
