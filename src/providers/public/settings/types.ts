f1eaf5979cccdbc2c6fda7cdf63bd3c59b42c137
ap211unitech <porwal.1@iitj.ac.in>
2023-04-11 14:42:59 +0530

    Setting type defined

---
+++ src/providers/public/settings/types.ts
@@ -0,0 +1,59 @@
+import { FC, PropsWithChildren } from "react";
+
+export type AlertTypes = "Error" | "Successful" | "Attention" | "Loading" | "Alert";
+
+export interface AlertState {
+  type?: AlertTypes;
+  status?: boolean;
+  message?: {
+    title?: string;
+    description?: string;
+  };
+}
+
+export type NotificationTypes =
+  | "InformationAlert"
+  | "ErrorAlert"
+  | "AttentionAlert"
+  | "SuccessAlert"
+  | "LoadingAlert";
+
+export interface Notification {
+  id?: number;
+  type?: NotificationTypes;
+  isRead?: boolean;
+  isActive?: boolean;
+  message?: {
+    title?: string;
+    description?: string;
+  };
+  time: number;
+  actionUrl?: string;
+  actionTitle?: string;
+  hasConfetti?: boolean;
+}
+
+export interface SettingState {
+  color: string;
+  chartRebuild: boolean;
+  ordersHideOtherPairs: boolean;
+  marketSelectorActive: boolean;
+  error: {
+    processing: boolean;
+  };
+  alert: AlertState;
+  notification: Notification[];
+}
+
+export type SettingProviderProps = PropsWithChildren<{
+  value: SettingContextProps;
+}>;
+
+export type SettingContextProps = SettingState & {};
+
+export interface SettingProps {
+  onError?: (value: string) => void;
+  onNotification?: (value: string) => void;
+}
+
+export type SettingComponent = FC<PropsWithChildren<SettingProps>>;
