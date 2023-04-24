29c7666d039dbde39f490fc9fe036026ed744650
Ruchi Ray <ruchiray18@gmail.com>
2023-04-24 09:56:21 +0530

    story for account bannner

---
+++ src/ui/molecules/AccountBanner/stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { AccountBanner} from ".";
+
+export default {
+  title: "Molecules/AccountBanner",
+  component: AccountBanner,
+  tags: ["autodocs"],
+} as Meta<typeof AccountBanner>;
+
+export const Default: StoryObj<typeof AccountBanner> = {
+  args: {
+    onClose: "test",
+  },
+};
