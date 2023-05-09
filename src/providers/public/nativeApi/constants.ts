0e72fc098e27e7a8d0098128446de6cd3eba24b3
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-05-09 10:53:26 -0300

    chore: 🔧 update reconnect time const name

--- src/providers/public/nativeApi/constants.ts
+++ src/providers/public/nativeApi/constants.ts
@@ -3,4 +3,4 @@ export const NATIVEAPI_CONNECT_DATA = "nativeapi/CONNECT_DATA";
 export const NATIVEAPI_CONNECT_ERROR = "nativeapi/CONNECT_ERROR";
 export const NATIVEAPI_DISCONNECT_FETCH = "nativeapi/DISCONNECT_FETCH";
 export const NATIVEAPI_DISCONNECT_DATA = "nativeapi/DISCONNECT_DATA";
-export const RECONNECT_TIME = 30000;
+export const RECONNECT_TIME_MS = 30000;
