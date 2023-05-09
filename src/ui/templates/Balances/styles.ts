5c6a292a6aa5e55dc33cfe6c161f9d3acc85a0ce
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-05-09 16:07:34 -0300

    chore: 🔧 empty result and filters

--- src/ui/templates/Balances/styles.ts
+++ src/ui/templates/Balances/styles.ts
@@ -26,6 +26,7 @@ export const ContainerMain = styled.div`
   align-items: center;
   width: 100%;
   padding-top: 4rem;
+  max-width: 100vw;
 `;
 
 export const Title = styled.div`
@@ -44,25 +45,32 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     border: 1px solid ${theme.colors.secondaryBackgroundOpacity};
-    padding: 1.5rem;
     border-radius: 1rem;
     width: 90%;
     max-width: 140rem;
   `}
 `;
 export const Content = styled.div`
-  overflow-x: auto;
+  padding: 1.5rem;
+  overflow-x: hidden;
+  :is(:hover) {
+    overflow-x: auto;
+  }
+  td,
+  th {
+    padding-right: 2rem;
+  }
 `;
 
 export const Header = styled.div`
   ${({ theme }) =>
     css`
+      padding: 1.5rem;
       display: flex;
       justify-content: space-between;
       align-items: center;
       gap: 1rem;
       padding-bottom: 1.5rem;
-      margin-bottom: 1.5rem;
       border-bottom: 1px solid ${theme.colors.secondaryBackgroundOpacity};
       h2 {
         font-size: 1.6rem;
@@ -97,6 +105,9 @@ export const Cell = styled.div`
     small {
       font-size: 1.3rem;
       color: ${theme.colors.tertiaryText};
+    }
+    span {
+      white-space: nowrap;
     }
   `}
 `;
