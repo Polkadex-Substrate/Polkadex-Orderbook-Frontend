d2ed417ad55c564600cb1b2bd51b95babef36cd1
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2023-05-19 15:23:18 +0530

    Working on Trading View

--- src/ui/molecules/TradingView/index.tsx
+++ src/ui/molecules/TradingView/index.tsx
@@ -2,16 +2,16 @@ import { createChart, ColorType } from "lightweight-charts";
 import React, { useEffect, useRef } from "react";
 
 const initialData = [
-  { time: "2018-12-22", value: 32.51 },
-  { time: "2018-12-23", value: 31.11 },
-  { time: "2018-12-24", value: 27.02 },
-  { time: "2018-12-25", value: 27.32 },
-  { time: "2018-12-26", value: 25.17 },
-  { time: "2018-12-27", value: 28.89 },
-  { time: "2018-12-28", value: 25.46 },
-  { time: "2018-12-29", value: 23.92 },
-  { time: "2018-12-30", value: 22.68 },
-  { time: "2018-12-31", value: 22.67 },
+  { open: 10, high: 10.63, low: 9.49, close: 9.55, time: 1642427876 },
+  { open: 9.55, high: 10.3, low: 9.42, close: 9.94, time: 1642514276 },
+  { open: 9.94, high: 10.17, low: 9.92, close: 9.78, time: 1642600676 },
+  { open: 9.78, high: 10.59, low: 9.18, close: 9.51, time: 1642687076 },
+  { open: 9.51, high: 10.46, low: 9.1, close: 10.17, time: 1642773476 },
+  { open: 10.17, high: 10.96, low: 10.16, close: 10.47, time: 1642859876 },
+  { open: 10.47, high: 11.39, low: 10.4, close: 10.81, time: 1642946276 },
+  { open: 10.81, high: 11.6, low: 10.3, close: 10.75, time: 1643032676 },
+  { open: 10.75, high: 11.6, low: 10.49, close: 10.93, time: 1643119076 },
+  { open: 10.93, high: 11.53, low: 10.76, close: 10.96, time: 1643205476 },
 ];
 
 export const TradingView = () => {
@@ -22,7 +22,7 @@ export const ChartComponent = (props) => {
   const {
     data,
     colors: {
-      backgroundColor = "black",
+      backgroundColor = "#1C1C26",
       lineColor = "#2962FF",
       textColor = "white",
       areaTopColor = "#2962FF",
@@ -33,6 +33,7 @@ export const ChartComponent = (props) => {
   const chartContainerRef = useRef();
 
   useEffect(() => {
+    console.log(chartContainerRef?.current, "chartContainerRef?.current?");
     const handleResize = () => {
       chart.applyOptions({ width: chartContainerRef?.current?.clientWidth });
     };
@@ -43,14 +44,16 @@ export const ChartComponent = (props) => {
         textColor,
       },
       width: chartContainerRef?.current?.clientWidth,
-      height: 400,
+      height: 440,
     });
     chart.timeScale().fitContent();
 
-    const newSeries = chart.addAreaSeries({
-      lineColor,
-      topColor: areaTopColor,
-      bottomColor: areaBottomColor,
+    const newSeries = chart.addCandlestickSeries({
+      upColor: "#26a69a",
+      downColor: "#ef5350",
+      borderVisible: false,
+      wickUpColor: "#26a69a",
+      wickDownColor: "#ef5350",
     });
     newSeries.setData(data);
 
