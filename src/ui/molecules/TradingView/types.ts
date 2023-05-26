f6ecf68078a8b023952a4e3f74452c6f5be3b897
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2023-05-26 13:51:40 +0530

    Code CleanUp

--- src/ui/molecules/TradingView/types.ts
+++
@@ -1,8 +0,0 @@
-export interface CurrentDataType {
-  timestamp: string | undefined;
-  close: number | undefined;
-  open: number | undefined;
-  high: number | undefined;
-  low: number | undefined;
-  volume: number | undefined;
-}
