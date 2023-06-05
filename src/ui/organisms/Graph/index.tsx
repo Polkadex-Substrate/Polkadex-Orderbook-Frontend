dac2914f24d00f935908bc911ced3248bc798bea
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-06-05 10:35:16 -0300

    chore: 🔧 dynamic TradinView component

--- src/ui/organisms/Graph/index.tsx
+++ src/ui/organisms/Graph/index.tsx
@@ -1,26 +1,28 @@
 import Script from "next/script";
 import { useState } from "react";
+import dynamic from "next/dynamic";
 
 import * as S from "./styles";
 
 import { OrderBook } from "@polkadex/orderbook-ui/organisms";
-import { TradingView } from "@polkadex/orderbook-ui/molecules";
+import { Keyboard } from "@polkadex/orderbook-ui/molecules/LoadingIcons";
+
+const TradingView = dynamic(
+  () => import("@polkadex/orderbook-ui/molecules").then((mod) => mod.TradingView),
+  {
+    ssr: false,
+  }
+);
 
 export const Graph = () => {
-  const [isScriptReady, setIsScriptReady] = useState(false);
-
   return (
     <>
-      <Script
-        src="/static/datafeeds/udf/dist/bundle.js"
-        strategy="lazyOnload"
-        onReady={() => {
-          setIsScriptReady(true);
-        }}
-      />
+      <Script src="/static/datafeeds/udf/dist/bundle.js" strategy="lazyOnload" />
       <S.Wrapper>
         <S.WrapperGraph>
-          <S.ChartWrapper>{isScriptReady && <TradingView />}</S.ChartWrapper>
+          <S.ChartWrapper>
+            <TradingView />
+          </S.ChartWrapper>
         </S.WrapperGraph>
         <OrderBook />
       </S.Wrapper>
