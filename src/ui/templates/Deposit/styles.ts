1b8509ed3af099b3af61cd7f2595045b668da1c2
minikas <kas_pintxuki@outlook.com>
2023-07-04 20:22:42 -0300

    update: fix deposit history box

--- src/ui/templates/Deposit/styles.ts
+++ src/ui/templates/Deposit/styles.ts
@@ -5,7 +5,7 @@ export const Main = styled.main`
     position: relative;
     background: ${theme.colors.primaryBackground};
     min-width: 100vw;
-    min-height: 100vh;
+    height: 100vh;
     display: flex;
     max-width: 160rem;
     box-shadow: 0px -36px 99px rgba(0, 0, 0, 0.15);
@@ -16,6 +16,7 @@ export const Flex = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column-reverse;
+  overflow: hidden;
   @media screen and (min-width: 590px) {
     flex-direction: row;
   }
@@ -25,6 +26,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   flex: 1;
   padding: 2rem;
+  overflow-y: scroll;
   @media screen and (min-width: 590px) {
     padding: 4rem 4rem 10rem 4rem;
   }
@@ -254,9 +256,12 @@ export const DropdownContent = styled.div`
 
 export const History = styled.div`
   ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
     background: ${theme.colors.secondaryBackgroundOpacity};
     border-radius: 1rem;
     margin-top: 4rem;
+    padding-bottom: 3rem;
     flex: 1;
     h2 {
       font-size: 1.8rem;
