609fec07b973ada5f7661db1761941668d92e671
RuchiRay <ruchiray18@gmail.com>
2023-07-05 12:41:32 +0530

    added use click outside hook

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+const useClickOutside = (ref, callback) => {
+  console.log("out");
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [callback, ref]);
+};
+
+export default useClickOutside;
