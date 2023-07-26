479272226e76ac0168160ad56c14efd6b56554ca
RuchiRay <ruchiray18@gmail.com>
2023-07-26 15:14:53 +0530

    added another check

--- src/pages/trading/index.tsx
+++ src/pages/trading/index.tsx
@@ -1,4 +1,10 @@
+import { useMarketsProvider } from "@polkadex/orderbook/providers/public/marketsProvider/useMarketsProvider";
+
 function Home() {
+  console.log("home page");
+  const { currentMarket } = useMarketsProvider();
+  console.log(currentMarket, "current market");
+
   return <div />;
 }
 
