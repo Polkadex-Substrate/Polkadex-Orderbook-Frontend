1b484e25dea140d2e133c0efafe28f5e21c44aec
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2023-08-03 18:12:58 +0530

    Klines event data fix

--- src/providers/public/marketsProvider/provider.tsx
+++ src/providers/public/marketsProvider/provider.tsx
@@ -97,6 +97,7 @@ export const MarketsProvider: MarketsComponent = ({ children }) => {
               }
             );
           }
+
         }
       } catch (error) {
         console.log(error, "error in fetching markets");
@@ -130,7 +131,7 @@ export const MarketsProvider: MarketsComponent = ({ children }) => {
     const to = new Date().toISOString();
     // tickers are fetched for the last 24 hours
     const from = new Date(new Date().setDate(new Date().getDate() - 1)).toISOString();
-    const res = await sendQueryToAppSync({
+    const res: any = await sendQueryToAppSync({
       query: queries.getMarketTickers,
       variables: { market, from, to },
     });
@@ -185,15 +186,7 @@ export const MarketsProvider: MarketsComponent = ({ children }) => {
         );
 
         const tickerData: Ticker = convertToTicker(dataParsed, state.currentMarket.m);
-
-        dispatch(
-          A.marketsTickersChannelData({
-            ...tickerData,
-            volumeBase24hr: isNaN(Number(tickerData.volumeBase24hr))
-              ? 0
-              : Number(tickerData.volumeBase24hr),
-          })
-        );
+        dispatch(A.marketsTickersChannelData(tickerData));
       },
       error: (err) => {
         console.warn(err);
