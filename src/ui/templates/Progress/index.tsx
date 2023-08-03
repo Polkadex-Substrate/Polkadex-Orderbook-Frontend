307e85280afd207289e82969f0e45af93f6bff6f
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-08-03 11:35:13 -0300

    update text

--- src/ui/templates/Progress/index.tsx
+++ src/ui/templates/Progress/index.tsx
@@ -40,8 +40,8 @@ export const Progress = () => {
               <S.HeroContent>
                 <h3>Test trading?</h3>
                 <p>
-                  If you want to be one of the first people to check Polkadex Orderbook and
-                  THEA out and start trading, stay tuned in{" "}
+                  If you want to be one of the first people to try Polkadex Orderbook and THEA
+                  and start trading, visit{" "}
                   <a href="https://discord.gg/h876ChKaNZ" target="_blank" rel="noreferrer">
                     the ambassador
                   </a>{" "}
@@ -49,7 +49,7 @@ export const Progress = () => {
                   <a href="https://discord.gg/HRx8ZzbtQy" target="_blank" rel="noreferrer">
                     validator channels
                   </a>{" "}
-                  on Discord.
+                  on the Polkadex Discord server..
                 </p>
               </S.HeroContent>
             </S.HeroHeader>
@@ -83,11 +83,11 @@ export const Progress = () => {
                   <Card checked>
                     <strong>Orderbook and THEA are live</strong> on the Polkadex network.
                   </Card>
-                  <Card pending>
+                  <Card checked>
                     DOT and USDT from the Relay Chain and AssetHub can now be deposited to
                     Polkadex via the Polkadex parachain and traded on Polkadex Orderbook.
                   </Card>
-                  <Card pending>
+                  <Card checked>
                     Polkadex validators and ambassadors can now start making trades with real
                     assets.
                   </Card>
