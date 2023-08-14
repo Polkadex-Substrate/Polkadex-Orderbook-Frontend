23a829445f38efb1d69ecce68d5d391923847167
RuchiRay <ruchiray18@gmail.com>
2023-08-14 16:15:20 +0530

    removed all the unused helper functions

--- src/helpers/sortByPrice.ts
+++
@@ -1,7 +0,0 @@
-export const sortAsks = (asks: string[][]) => {
-  return asks.sort((a, b) => Number(a[0]) - Number(b[0]));
-};
-
-export const sortBids = (bids: string[][]) => {
-  return bids.sort((a, b) => Number(b[0]) - Number(a[0]));
-};
