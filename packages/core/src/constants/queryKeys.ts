06dbe48519872732f4b985e7e4876a2351fc2848
nuel77 <emmanuel.thomas237@gmail.com>
2023-09-19 15:39:06 +0300

    add query keys

---
+++ packages/core/src/constants/queryKeys.ts
@@ -0,0 +1,10 @@
+import { Maybe } from "@orderbook/core/helpers";
+
+const PREFIX = "@orderbook";
+export const QUERY_KEYS = {
+  blockchainTransfers: (id: Maybe<string>) => [
+    PREFIX,
+    "blockchainTransfers",
+    id,
+  ],
+};
