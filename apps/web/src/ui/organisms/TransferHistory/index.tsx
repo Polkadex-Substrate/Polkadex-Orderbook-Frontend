1b560bc974c0d8a5d452d8b2f5cce125460d126d
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-09-22 10:36:30 -0300

    update: fix table filters

--- apps/web/src/ui/organisms/TransferHistory/index.tsx
+++ apps/web/src/ui/organisms/TransferHistory/index.tsx
@@ -147,14 +147,19 @@ export const TransferHistory = ({
                       asc: getSorted === "asc",
                       desc: getSorted === "desc",
                     });
+                    const handleSort = () => {
+                      const isDesc = getSorted === "desc";
+                      header.column.toggleSorting(!isDesc);
+                    };
+                    const isActionTab = header.id === "date";
+                    const theadProps = isActionTab
+                      ? { onClick: handleSort }
+                      : {};
                     return (
                       <S.Thead
                         key={header.id}
                         className={trClassName}
-                        onClick={() => {
-                          const isDesc = getSorted === "desc";
-                          header.column.toggleSorting(!isDesc);
-                        }}
+                        {...theadProps}
                       >
                         {header.isPlaceholder
                           ? null
@@ -162,9 +167,11 @@ export const TransferHistory = ({
                               header.column.columnDef.header,
                               header.getContext()
                             )}
-                        <div>
-                          <Icons.IncreaseFilter />
-                        </div>
+                        {isActionTab && (
+                          <div>
+                            <Icons.IncreaseFilter />
+                          </div>
+                        )}
                       </S.Thead>
                     );
                   })}
