76bdb6e2ba321d17b9c2ccbfdf4e77ac08a19161
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-09-28 15:02:12 -0300

    update: transitions

--- apps/web/src/ui/templates/Landing/index.tsx
+++ apps/web/src/ui/templates/Landing/index.tsx
@@ -2,10 +2,12 @@ import localFont from "next/font/local";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import { Fade, Slide } from "react-reveal";
 
 import SpeedImage from "../../../../public/img/speed.webp";
 import FeesImage from "../../../../public/img/fees.webp";
 import MarketImage from "../../../../public/img/market.webp";
+import NonCustodial from "../../../../public/img/nonCustodial.webp";
 
 import * as S from "./styles";
 
@@ -24,206 +26,247 @@ export function LandingTemplate() {
       </Head>
       <S.Main>
         <Header dark />
-        <S.Hero>
-          <S.HeroAside>
-            <S.HeroHeader>
-              <span>
-                <Icons.Checked />
-                Non-custodial
-              </span>
-              <span>
-                <Icons.Checked />
-                Zero gas fees
-              </span>
-              <span>
-                <Icons.Checked />
-                500k TPS performance
-              </span>
-            </S.HeroHeader>
-            <h1>Trade on the CEXiest decentralized</h1>
-            <p className={outrun.className}>
-              Exchange <strong>.</strong>
-            </p>
-            <S.Button href="/trading">Start trading</S.Button>
-          </S.HeroAside>
-        </S.Hero>
+        <Fade>
+          <S.Hero>
+            <S.HeroAside>
+              <S.HeroHeader>
+                <span>
+                  <Icons.Checked />
+                  Non-custodial
+                </span>
+                <span>
+                  <Icons.Checked />
+                  Zero gas fees
+                </span>
+                <span>
+                  <Icons.Checked />
+                  500k TPS performance
+                </span>
+              </S.HeroHeader>
+              <h1>Trade on the CEXiest decentralized</h1>
+              <p className={outrun.className}>
+                Exchange <strong>.</strong>
+              </p>
+              <S.Button href="/trading">Start trading</S.Button>
+            </S.HeroAside>
+          </S.Hero>
+        </Fade>
         <S.Start>
           <S.StartHeader>
-            <span className={outrun.className}>Fast</span>
-            <span className={outrun.className}>Simple</span>
-            <span className={outrun.className}>Secure</span>
+            <Slide bottom>
+              <span className={outrun.className}>Fast</span>
+            </Slide>
+            <Slide bottom>
+              <span className={outrun.className}>Simple</span>
+            </Slide>
+            <Slide bottom>
+              <span className={outrun.className}>Secure</span>
+            </Slide>
           </S.StartHeader>
-          <S.StartContent>
-            <S.StartCard>
-              <span />
-              <div>
-                <h3>1.Create an account</h3>
-                <p>
-                  Sign up on Orderbook. Rest assured, we prioritize your
-                  security and use any data solely for security and marketing.
-                </p>
-              </div>
-            </S.StartCard>
-            <S.StartCard>
-              <span />
-              <div>
-                <h3>2.Connect your wallet</h3>
-                <p>
-                  Choose your wallet from a variety of Polkadot-based wallets,
-                  and register it for use within Orderbook.
-                </p>
-              </div>
-            </S.StartCard>
-            <S.StartCard>
-              <span />
-              <div>
-                <h3>3.Add funds & start trading</h3>
-                <p>
-                  Transfer funds into your Orderbook wallet. Now, you&lsquo;re
-                  all set to start trading.
-                </p>
-              </div>
-            </S.StartCard>
-          </S.StartContent>
-          <S.StartFooter>
-            <span>
-              Click this small button to begin trading
-              <Icons.SingleArrowRight />
-            </span>
-            <S.Button href="/trading">Start trading</S.Button>
-          </S.StartFooter>
-        </S.Start>
-        <S.Features>
-          <S.FeaturesHeader>
-            <S.FeatureShadow />
-            <S.FeaturesHeaderCard>
-              <div>
-                <span className={outrun.className}>Your</span>
-                <p className={outrun.className}>Keys</p>
-              </div>
-              <div>
-                <span className={outrun.className}>Your</span>
-                <p className={outrun.className}>Crypto</p>
-              </div>
-              <div>
-                <span className={outrun.className}>Your</span>
-                <p className={outrun.className}>Exchange</p>
-              </div>
-            </S.FeaturesHeaderCard>
-          </S.FeaturesHeader>
-          <S.FeaturesContent>
-            <S.FeaturesHighlight>
-              <div>
-                <h3>500k trades/sec</h3>
-                <p>
-                  Trade crypto with sub-milisecond latency on a DEX that’s as
-                  fast as CEXs.
-                </p>
-              </div>
-              <Image
-                src={SpeedImage}
-                placeholder="blur"
-                alt="Speed illustration"
-                priority
-                draggable={false}
-                style={{
-                  width: "100%",
-                  height: "auto",
-                }}
-              />
-            </S.FeaturesHighlight>
-            <S.FeaturesWrapper>
-              <S.FeaturesCard>
+          <Slide bottom>
+            <S.StartContent>
+              <S.StartCard>
+                <span />
                 <div>
-                  <h3>Ø Trading fees</h3>
+                  <h3>1.Create an account</h3>
                   <p>
                     Sign up on Orderbook. Rest assured, we prioritize your
                     security and use any data solely for security and marketing.
                   </p>
                 </div>
+              </S.StartCard>
+              <S.StartCard>
+                <span />
+                <div>
+                  <h3>2.Connect your wallet</h3>
+                  <p>
+                    Choose your wallet from a variety of Polkadot-based wallets,
+                    and register it for use within Orderbook.
+                  </p>
+                </div>
+              </S.StartCard>
+              <S.StartCard>
+                <span />
+                <div>
+                  <h3>3.Add funds & start trading</h3>
+                  <p>
+                    Transfer funds into your Orderbook wallet. Now, you&lsquo;re
+                    all set to start trading.
+                  </p>
+                </div>
+              </S.StartCard>
+            </S.StartContent>
+          </Slide>
+          <Slide bottom>
+            <S.StartFooter>
+              <span>
+                Click this small button to begin trading
+                <Icons.SingleArrowRight />
+              </span>
+              <S.Button href="/trading">Start trading</S.Button>
+            </S.StartFooter>
+          </Slide>
+        </S.Start>
+        <S.Features>
+          <S.FeaturesHeader>
+            <S.FeatureShadow />
+            <S.FeaturesHeaderCard>
+              <Slide bottom>
+                <div>
+                  <span className={outrun.className}>Your</span>
+                  <p className={outrun.className}>Keys</p>
+                </div>
+              </Slide>
+              <Slide bottom>
+                <div>
+                  <span className={outrun.className}>Your</span>
+                  <p className={outrun.className}>Crypto</p>
+                </div>
+              </Slide>
+              <Slide bottom>
+                <div>
+                  <span className={outrun.className}>Your</span>
+                  <p className={outrun.className}>Exchange</p>
+                </div>
+              </Slide>
+            </S.FeaturesHeaderCard>
+          </S.FeaturesHeader>
+          <S.FeaturesContent>
+            <Slide bottom>
+              <S.FeaturesHighlight>
+                <div>
+                  <h3>500k trades/sec</h3>
+                  <p>
+                    Trade crypto with sub-milisecond latency on a DEX that’s as
+                    fast as CEXs.
+                  </p>
+                </div>
                 <Image
-                  src={FeesImage}
+                  src={SpeedImage}
                   placeholder="blur"
-                  alt="Fees illustration"
+                  alt="Speed illustration"
+                  priority
+                  draggable={false}
                   style={{
                     width: "100%",
                     height: "auto",
                   }}
                 />
+              </S.FeaturesHighlight>
+            </Slide>
+            <S.FeaturesWrapper>
+              <S.FeaturesCard>
+                <Slide bottom>
+                  <div>
+                    <h3>Ø Trading fees</h3>
+                    <p>
+                      Sign up on Orderbook. Rest assured, we prioritize your
+                      security and use any data solely for security and
+                      marketing.
+                    </p>
+                  </div>
+                  <Image
+                    src={FeesImage}
+                    placeholder="blur"
+                    alt="Fees illustration"
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                    }}
+                  />
+                </Slide>
               </S.FeaturesCard>
               <S.FeaturesCard>
-                <div>
-                  <h3>Limit & market orders</h3>
-                  <p>
-                    Control price with limit orders, achieve instant execution
-                    with market orders.
-                  </p>
-                </div>
-                <Image
-                  src={MarketImage}
-                  placeholder="blur"
-                  alt="Market illustration"
-                  style={{
-                    width: "100%",
-                    height: "auto",
-                  }}
-                />
+                <Slide bottom>
+                  <div>
+                    <h3>Limit & market orders</h3>
+                    <p>
+                      Control price with limit orders, achieve instant execution
+                      with market orders.
+                    </p>
+                  </div>
+                  <Image
+                    src={MarketImage}
+                    placeholder="blur"
+                    alt="Market illustration"
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                    }}
+                  />
+                </Slide>
               </S.FeaturesCard>
               <S.FeaturesCard>
-                <div>
-                  <h3>Non-custodial</h3>
-                  <p>
-                    You control your own liquidity, which means it is safe, and
-                    you can pull it out whenever.
-                  </p>
-                </div>
-                <img src="/img/nonCustodial.png" />
+                <Slide bottom>
+                  <div>
+                    <h3>Non-custodial</h3>
+                    <p>
+                      You control your own liquidity, which means it is safe,
+                      and you can pull it out whenever.
+                    </p>
+                  </div>
+                  <Image
+                    src={NonCustodial}
+                    placeholder="blur"
+                    alt="NonCustodial illustration"
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                    }}
+                  />
+                </Slide>
               </S.FeaturesCard>
             </S.FeaturesWrapper>
-            <S.FeaturesFooter>
-              <Link href="https://polkadex.trade/orderbook" target="_blank">
-                Read more about Polkadex Orderbook
-                <Icons.SingleArrowRight />
-              </Link>
-              <S.Button href="/trading">Start trading</S.Button>
-            </S.FeaturesFooter>
+            <Slide bottom>
+              <S.FeaturesFooter>
+                <Link href="https://polkadex.trade/orderbook" target="_blank">
+                  Read more about Polkadex Orderbook
+                  <Icons.SingleArrowRight />
+                </Link>
+                <S.Button href="/trading">Start trading</S.Button>
+              </S.FeaturesFooter>
+            </Slide>
           </S.FeaturesContent>
         </S.Features>
         <S.Support>
           <S.SupportCard>
-            <S.SupportCardHeader>
-              <h3>FAQs</h3>
-              <p>
-                Explore our Frequently Asked Questions section for in-depth
-                guidance on specific features.
-              </p>
-            </S.SupportCardHeader>
-            <Link href="/">
-              Explore FAQ
-              <Icons.SingleArrowRight />
-            </Link>
+            <Slide bottom>
+              <S.SupportCardHeader>
+                <h3>FAQs</h3>
+                <p>
+                  Explore our Frequently Asked Questions section for in-depth
+                  guidance on specific features.
+                </p>
+              </S.SupportCardHeader>
+              <Link href="/">
+                Explore FAQ
+                <Icons.SingleArrowRight />
+              </Link>
+            </Slide>
           </S.SupportCard>
           <S.SupportCard>
-            <S.SupportCardHeader>
-              <h3>Join Our Community</h3>
-              <p>
-                Polkadex Community connects users from 100+ countries and
-                supporting 5+ languages.
-              </p>
-            </S.SupportCardHeader>
-            <S.SupportCardFooter>
-              <Link
-                href="https://discord.com/invite/Uvua83QAzk"
-                target="_blank"
-              >
-                <Icons.Discord />
-                Discover
-              </Link>
-              <Link href="https://t.me/Polkadex" target="_blank">
-                <Icons.Telegram />
-                Telegram
-              </Link>
-            </S.SupportCardFooter>
+            <Slide bottom>
+              <S.SupportCardHeader>
+                <h3>Join Our Community</h3>
+                <p>
+                  Polkadex Community connects users from 100+ countries and
+                  supporting 5+ languages.
+                </p>
+              </S.SupportCardHeader>
+              <S.SupportCardFooter>
+                <Link
+                  href="https://discord.com/invite/Uvua83QAzk"
+                  target="_blank"
+                >
+                  <Icons.Discord />
+                  Discover
+                </Link>
+                <Link href="https://t.me/Polkadex" target="_blank">
+                  <Icons.Telegram />
+                  Telegram
+                </Link>
+              </S.SupportCardFooter>
+            </Slide>
           </S.SupportCard>
         </S.Support>
         <S.Footer>
