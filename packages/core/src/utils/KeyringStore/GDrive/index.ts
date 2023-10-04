c6f40875830a5f2ff7035b861c09b852fc603de0
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2023-10-04 15:30:54 +0530

    basic scaffold

---
+++ packages/core/src/utils/KeyringStore/GDrive/index.ts
@@ -0,0 +1,19 @@
+import {KeyringJson, KeyringStore} from "@polkadot/ui-keyring/types";
+import {string} from "yup";
+import keyring from "@polkadot/ui-keyring";
+
+keyring.addHardware()
+export default class GDriveAccountsStore implements KeyringStore {
+    all(cb: (key: string, value: KeyringJson) => void): void {
+
+    }
+
+    get(key: string, cb: (value: KeyringJson) => void): void {
+    }
+
+    remove(key: string, cb: (() => void) | undefined): void {
+    }
+
+    set(key: string, value: KeyringJson, cb: (() => void) | undefined): void {
+    }
+}
