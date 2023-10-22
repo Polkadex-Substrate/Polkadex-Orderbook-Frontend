7d687584116921b76b8ef35f80a1b94bba5cd7d9
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2023-10-22 22:46:15 +0530

    update: fixed balance formatting

--- packages/core/src/helpers/formatting.ts
+++ packages/core/src/helpers/formatting.ts
@@ -4,8 +4,9 @@ const DIGITS_AFTER_DECIMAL = 4;
 
 // TODO: Need to make it more configurable
 export const formatBalances = (value: string) => {
+  value = Number(value).toFixed(20);
   let balance = trimFloat({
-    value: Number(value),
+    value,
     digitsAfterDecimal: DIGITS_AFTER_DECIMAL,
   });
   balance = formatNumber(balance);
