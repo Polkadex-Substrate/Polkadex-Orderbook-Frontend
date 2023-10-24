a7a4d81720d5a8ff51ce3b3acab93302ec02c256
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-10-24 11:58:30 -0300

    update: fix tradingview

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -35,6 +35,10 @@ const nextConfig = {
         source: "/(.*)?",
         headers: [
           {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
