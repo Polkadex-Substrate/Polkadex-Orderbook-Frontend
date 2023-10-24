4208363f69c1f26c7c29f157006a4e4b2d8d000b
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2023-10-24 19:31:58 +0530

    fix nova graph issue

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -35,10 +35,6 @@ const nextConfig = {
         source: "/(.*)?",
         headers: [
           {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
