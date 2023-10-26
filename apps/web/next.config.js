a390d0aa306b0b1dfac70849fc4afac9f62b92ad
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2023-10-27 01:57:10 +0530

    fix mime issue

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -27,7 +27,7 @@ const nextConfig = {
       config.externals.push("_http_common");
     }
     config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
-    config.output = { ...config.output, ...{ path: "/" } };
+    config.output = { ...config.output, ...{ publicPath: "/" } };
     return config;
   },
   async headers() {
