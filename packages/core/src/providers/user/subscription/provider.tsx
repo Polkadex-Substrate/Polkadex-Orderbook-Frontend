2e2925f77bd0fd9cbf3511fb39575b07e9bc765c
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2023-12-11 10:31:31 +0530

    feat: added open orders realtime subscription in provider

--- packages/core/src/providers/user/subscription/provider.tsx
+++ packages/core/src/providers/user/subscription/provider.tsx
@@ -1,16 +1,22 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useRouter } from "next/router";
 import { usePathname } from "next/navigation";
 import {
   PublicTrade,
   appsyncOrderbookService,
+  Order,
 } from "@orderbook/core/utils/orderbookService";
 import { useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@orderbook/core/constants";
 import { useOrderbookService } from "@orderbook/core/providers/public/orderbookServiceProvider/useOrderbookService";
 import { getCurrentMarket } from "@orderbook/core/helpers";
+import {
+  removeOrderFromList,
+  replaceOrPushOrder,
+} from "@orderbook/core/utils/orderbookService/appsync_v1/helpers";
 
 import { useProfile } from "../profile";
+import { useSettingsProvider } from "../../public/settings";
 
 import { Provider } from "./context";
 import * as T from "./types";
@@ -19,35 +25,78 @@ export const SubscriptionProvider: T.SubscriptionComponent = ({ children }) => {
   const queryClient = useQueryClient();
   const path = usePathname();
   const router = useRouter();
+  const { onHandleError } = useSettingsProvider();
   const { isReady, markets } = useOrderbookService();
   const {
-    selectedAccount: { tradeAddress, mainAddress },
+    selectedAccount: { tradeAddress },
   } = useProfile();
 
   const isTradingPage = path.startsWith("/trading");
   const marketName = isTradingPage ? (router.query.id as string) : null;
   const market = getCurrentMarket(markets, marketName)?.id;
 
-  // Recent trades subscription
+  const onOrderUpdates = useCallback(
+    (payload: Order) => {
+      try {
+        // Update OpenOrders Realtime
+        queryClient.setQueryData(
+          QUERY_KEYS.openOrders(tradeAddress),
+          (oldOpenOrders: Order[]) => {
+            const prevOpenOrders = [...oldOpenOrders];
+
+            let updatedOpenOrders: Order[] = [];
+            if (payload.status === "OPEN") {
+              updatedOpenOrders = replaceOrPushOrder(prevOpenOrders, payload);
+            } else {
+              // Remove from Open Orders if it is closed
+              updatedOpenOrders = removeOrderFromList(prevOpenOrders, payload);
+            }
+            return updatedOpenOrders;
+          }
+        );
+      } catch (error) {
+        onHandleError(`Order updates channel ${error?.message ?? error}`);
+      }
+    },
+    [onHandleError, queryClient, tradeAddress]
+  );
+
+  const onRecentTradeUpdates = useCallback(
+    (trade: PublicTrade) => {
+      if (market) {
+        queryClient.setQueryData(QUERY_KEYS.recentTrades(market), (oldData) => {
+          const oldRecentTrades = oldData as PublicTrade[];
+          return [trade, ...oldRecentTrades];
+        });
+      }
+    },
+    [market, queryClient]
+  );
+
+  // Recent Trades subscription
   useEffect(() => {
     if (!isReady || !market || market?.length <= 0) return;
 
     const subscription =
       appsyncOrderbookService.subscriber.subscribeLatestTrades(
         market,
-        (trade: PublicTrade) => {
-          queryClient.setQueryData(
-            QUERY_KEYS.recentTrades(market),
-            (oldData) => {
-              const oldRecentTrades = oldData as PublicTrade[];
-              return [trade, ...oldRecentTrades];
-            }
-          );
-        }
+        onRecentTradeUpdates
       );
 
     return () => subscription.unsubscribe();
-  }, [isReady, market, queryClient]);
+  }, [isReady, market, onRecentTradeUpdates]);
+
+  // Open Orders subscription
+  useEffect(() => {
+    if (tradeAddress?.length && isReady) {
+      const subscription = appsyncOrderbookService.subscriber.subscribeOrders(
+        tradeAddress,
+        onOrderUpdates
+      );
+
+      return () => subscription.unsubscribe();
+    }
+  }, [tradeAddress, onOrderUpdates, isReady]);
 
   return <Provider value={{}}>{children}</Provider>;
 };
