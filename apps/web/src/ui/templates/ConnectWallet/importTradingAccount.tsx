0a27213353c512dbc2d74a835cada4d5182ab40c
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-12-11 14:40:05 -0300

    fix: importTradingAccount whiteList fixes

--- apps/web/src/ui/templates/ConnectWallet/importTradingAccount.tsx
+++ apps/web/src/ui/templates/ConnectWallet/importTradingAccount.tsx
@@ -3,31 +3,51 @@ import {
   EyeSlashIcon,
   FolderPlusIcon,
 } from "@heroicons/react/24/outline";
-import { useEffect, useMemo, useState } from "react";
-import { Button, Interaction, Typography } from "@polkadex/ux";
+import { useMemo, useState } from "react";
+import { Button, Input, Interaction, Loading, Typography } from "@polkadex/ux";
 import { useFormik } from "formik";
 import { useDropzone } from "react-dropzone";
 import classNames from "classnames";
 import { DecodedFile } from "@orderbook/core/providers/user/walletProvider";
 
 import { ErrorMessage, OptionalField } from "../ReadyToUse";
-import { Input } from "../Input";
 
 const initialValues = {
   file: null,
 };
 
 const initialState = ["", "", "", "", ""];
+
+const parseFile = (file: File): Promise<DecodedFile | string> =>
+  new Promise((resolve) => {
+    const blob = new Blob([file], {
+      type: "text/plain;charset=utf-8",
+    });
+    const reader = new FileReader();
+    reader.onload = () => {
+      try {
+        const decodedFile: DecodedFile = JSON.parse(String(reader.result));
+        resolve(decodedFile);
+      } catch (error) {
+        resolve("Error parsing file");
+      }
+    };
+    reader.onerror = () => resolve("Error reading file");
+    reader.readAsText(blob);
+  });
+
 export const ImportTradingAccount = ({
-  onBack,
+  loading,
   onClose,
-  onAction,
+  onImport,
   onRedirect,
+  whitelistAccounts,
 }: {
-  onBack: () => void;
   onClose: () => void;
-  onAction: (values: { password: string; file: DecodedFile }) => void;
+  onImport: (values: { password: string; file: DecodedFile }) => Promise<void>;
   onRedirect: () => void;
+  loading: boolean;
+  whitelistAccounts?: string[];
 }) => {
   const [show, setShow] = useState(false);
   const [error, setError] = useState("");
@@ -39,13 +59,12 @@ export const ImportTradingAccount = ({
       file: DecodedFile | null;
     }>({
       initialValues,
-      onSubmit: ({ file }) => {
+      onSubmit: async ({ file }) => {
         try {
           const password = state.join("");
-          onAction({ file: file as DecodedFile, password });
+          await onImport({ file: file as DecodedFile, password });
           onRedirect();
         } catch (error) {
-          console.log("error..", error);
           setError((error as Error)?.message ?? error);
           resetForm();
           setState(initialState);
@@ -53,89 +72,87 @@ export const ImportTradingAccount = ({
       },
     });
 
-  const hasFile = useMemo(() => !!values.file, [values.file]);
-  const { getRootProps, getInputProps, isDragReject } = useDropzone({
-    maxFiles: 1,
-    accept: { "application/json": [".json"] },
-    onDrop: (acceptedFiles: File[]) => {
-      const blob = new Blob([acceptedFiles[0]], {
-        type: "text/plain;charset=utf-8",
-      });
-      const reader = new FileReader();
-      reader.readAsText(blob);
-      reader.onload = () => {
-        if (reader.result) {
-          const decodedFile: DecodedFile = JSON.parse(String(reader.result));
-          setFieldValue(
-            "file",
-            decodedFile?.address?.length ? decodedFile : ""
-          );
-        }
-      };
-    },
-  });
+  // Move to validation
+  // const isValidFile = useMemo(() => {
+  //   if (!whitelistAccounts) return true;
+  //   return (
+  //     values.file?.address && whitelistAccounts.includes(values.file?.address)
+  //   );
+  // }, [values.file?.address, whitelistAccounts]);
 
-  useEffect(() => {
-    if (error) setTimeout(() => setError(""), 5000);
-  }, [error]);
+  const hasFile = useMemo(() => !!values.file, [values.file]);
+  const { getRootProps, getInputProps, isDragReject, isDragAccept } =
+    useDropzone({
+      maxFiles: 1,
+      validator: (file) => null, // Async not working...
+      accept: { "application/json": [".json"] },
+      onDrop: async (acceptedFiles: File[]) => {
+        const decodedFile = await parseFile(acceptedFiles[0]);
+        setFieldValue("file", decodedFile || "");
+      },
+    });
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Interaction className="gap-10">
-        <Interaction.Title onBack={onBack} onClose={onClose}>
-          Import Account
-        </Interaction.Title>
-        <Interaction.Content className="flex-1">
-          {hasFile ? (
-            <div className="flex flex-col gap-5 flex-1">
-              <Input.Vertical
-                label="Account name"
-                defaultValue={values?.file?.meta.name}
-                placeholder="Account nae"
-              />
-              <OptionalField label="Protected by password?">
-                <div className="flex items-center justify-between">
-                  <Input.Passcode
-                    focusOnInit
-                    type={show ? "password" : "text"}
-                    values={state}
-                    onValuesChange={setState}
-                  />
-                  <Button.Icon variant="ghost" onClick={() => setShow(!show)}>
-                    {show ? <EyeIcon /> : <EyeSlashIcon />}
-                  </Button.Icon>
-                </div>
-              </OptionalField>
-            </div>
-          ) : (
-            <div
-              {...getRootProps({ className: "dropzone" })}
-              className={classNames(
-                isDragReject && "border-danger-base",
-                "flex flex-col items-center text-center gap-2 cursor-pointer py-8 px-3 rounded-md border-primary border-2 border-dashed hover:border-level-4 transition-colors duration-300"
-              )}
-            >
-              <input {...getInputProps()} />
-              <FolderPlusIcon className="w-7 h-7 text-primary" />
-              <div className="flex flex-col">
-                <Typography.Text>Choose a file</Typography.Text>
-                <Typography.Text variant="primary">
-                  or drag and drop it here
-                </Typography.Text>
+    <Loading.Spinner active={loading}>
+      <form onSubmit={handleSubmit}>
+        <Interaction className="gap-10">
+          <Interaction.Title onClose={onClose}>
+            Import Account
+          </Interaction.Title>
+          <Interaction.Content className="flex-1">
+            {hasFile ? (
+              <div className="flex flex-col gap-5 flex-1">
+                <Input.Vertical
+                  label="Account name"
+                  defaultValue={values?.file?.meta?.name}
+                  placeholder="Account name"
+                />
+                <OptionalField label="Protected by password?">
+                  <div className="flex items-center justify-between">
+                    <Input.Passcode
+                      focusOnInit
+                      type={show ? "password" : "text"}
+                      values={state}
+                      onValuesChange={setState}
+                      className="bg-level-4"
+                    />
+                    <Button.Icon variant="ghost" onClick={() => setShow(!show)}>
+                      {show ? <EyeIcon /> : <EyeSlashIcon />}
+                    </Button.Icon>
+                  </div>
+                </OptionalField>
               </div>
-              {(isDragReject || error) && (
-                <ErrorMessage>Invalid trading account</ErrorMessage>
-              )}
-            </div>
-          )}
-        </Interaction.Content>
-        <Interaction.Footer>
-          <Interaction.Action type="submit" disabled={!(isValid && dirty)}>
-            Import account
-          </Interaction.Action>
-          <Interaction.Close onClick={onBack}>Back</Interaction.Close>
-        </Interaction.Footer>
-      </Interaction>
-    </form>
+            ) : (
+              <div
+                {...getRootProps({ className: "dropzone" })}
+                className={classNames(
+                  isDragReject && "border-danger-base",
+                  isDragAccept && "border-success-base",
+                  "flex flex-col items-center text-center gap-2 cursor-pointer py-8 px-3 rounded-md border-level-5 border-2 border-dashed hover:border-level-4 transition-colors duration-300"
+                )}
+              >
+                <input {...getInputProps()} />
+                <FolderPlusIcon className="w-7 h-7 text-primary" />
+                <div className="flex flex-col">
+                  <Typography.Text>Choose a file</Typography.Text>
+                  <Typography.Text variant="primary">
+                    or drag and drop it here
+                  </Typography.Text>
+                </div>
+                {(isDragReject || error) && (
+                  <ErrorMessage>{error || "Invalid file"}</ErrorMessage>
+                )}
+              </div>
+            )}
+          </Interaction.Content>
+          <Interaction.Footer>
+            <Interaction.Action type="submit" disabled={!(isValid && dirty)}>
+              Import account
+            </Interaction.Action>
+            <Interaction.Close onClick={onClose}>Back</Interaction.Close>
+          </Interaction.Footer>
+        </Interaction>
+      </form>
+    </Loading.Spinner>
   );
 };
