5c29d20a3106443624da4334b5c766f613d397dd
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-12-12 09:30:46 -0300

    update: remove feedbackForm

--- apps/web/src/pages/faq/index.tsx
+++
@@ -1,9 +0,0 @@
-import FAQLayout from "@polkadex/orderbook-ui/organisms/FAQLayout";
-
-const Home = () => {
-  return <div>Home page screen</div>;
-};
-
-Home.getLayout = (page) => <FAQLayout>{page}</FAQLayout>;
-
-export default Home;
