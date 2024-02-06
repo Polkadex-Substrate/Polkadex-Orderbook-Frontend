c4255eef0bb789ace734736472820d9074436fea
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-02-06 09:42:58 -0300

    fix: removed query.id from substrictionProvider

--- apps/web/src/ui/templates/DynamicProviders/index.tsx
+++ apps/web/src/ui/templates/DynamicProviders/index.tsx
@@ -10,8 +10,10 @@ import {
   SubscriptionProvider,
   ConnectWalletProvider,
 } from "@orderbook/core/providers";
+import { useRouter } from "next/router";
 
 export const DynamicProviders = ({ children }) => {
+  const { query } = useRouter();
   return (
     <ExtensionsProvider>
       <ExtensionAccountsProvider
@@ -23,7 +25,7 @@ export const DynamicProviders = ({ children }) => {
           <ProfileProvider>
             <NativeApiProvider>
               <OrderbookServiceProvider>
-                <SubscriptionProvider>
+                <SubscriptionProvider marketId={query.id as string}>
                   <ConnectWalletProvider>{children}</ConnectWalletProvider>
                 </SubscriptionProvider>
               </OrderbookServiceProvider>
