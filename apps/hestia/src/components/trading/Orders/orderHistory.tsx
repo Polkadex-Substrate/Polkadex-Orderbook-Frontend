33db6b171c82810b1dbc77a5a7a745870d47bca9
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-02-08 16:16:13 +0530

    fix: implemented infinite scroll in order history

--- apps/hestia/src/components/trading/Orders/orderHistory.tsx
+++ apps/hestia/src/components/trading/Orders/orderHistory.tsx
@@ -1,4 +1,5 @@
 "use client";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { useMemo } from "react";
 import { useWindowSize } from "usehooks-ts";
 import {
@@ -7,7 +8,12 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import classNames from "classnames";
-import { GenericMessage, Table as PolkadexTable } from "@polkadex/ux";
+import {
+  Button,
+  GenericMessage,
+  Table as PolkadexTable,
+  Spinner,
+} from "@polkadex/ux";
 import { useOrderHistory } from "@orderbook/core/hooks";
 import { Ifilters } from "@orderbook/core/providers/types";
 
@@ -24,7 +30,8 @@ export const OrderHistoryTable = ({
   filters: Ifilters;
   maxHeight: string;
 }) => {
-  const { isLoading, orderHistory } = useOrderHistory(market, filters);
+  const { isLoading, orderHistory, error, hasNextPage, onFetchNextPage } =
+    useOrderHistory(market, filters);
   const { width } = useWindowSize();
   const table = useReactTable({
     data: orderHistory,
@@ -42,72 +49,80 @@ export const OrderHistoryTable = ({
   if (!orderHistory.length)
     return <GenericMessage title={"No items found"} illustration="NoData" />;
 
-  if (responsiveView) {
-    return (
-      <div
-        className="flex-1 overflow-y-hidden hover:overflow-y-auto"
-        style={{ maxHeight, scrollbarGutter: "stable" }}
-      >
-        <OrderHistoryResponsiveCard orders={orderHistory} />
-      </div>
-    );
-  }
-
   return (
-    <div
+    <InfiniteScroll
       className="flex-1 overflow-y-hidden hover:overflow-y-auto"
-      style={{ maxHeight, scrollbarGutter: "stable" }}
+      style={{ scrollbarGutter: "stable" }}
+      dataLength={orderHistory.length}
+      next={() => {
+        onFetchNextPage();
+      }}
+      hasMore={Boolean(hasNextPage)}
+      height={maxHeight}
+      loader={<Spinner.Keyboard className="h-6 mx-auto my-2" />}
     >
-      <PolkadexTable className="w-full">
-        <PolkadexTable.Header className="sticky top-0 bg-black">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <PolkadexTable.Row key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <PolkadexTable.Head
-                    className={classNames(
-                      "px-2 text-primary font-semibold text-xs"
-                    )}
-                    key={header.id}
-                  >
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                  </PolkadexTable.Head>
-                );
-              })}
-            </PolkadexTable.Row>
-          ))}
-        </PolkadexTable.Header>
-        <PolkadexTable.Body>
-          {table.getRowModel().rows.map((row, i) => {
-            return (
-              <PolkadexTable.Row
-                key={row.id}
-                className={classNames(
-                  "hover:bg-level-1 cursor-pointer",
-                  i % 2 && "bg-level-1"
-                )}
-              >
-                {row.getVisibleCells().map((cell) => {
+      {responsiveView ? (
+        <OrderHistoryResponsiveCard orders={orderHistory} />
+      ) : (
+        <PolkadexTable className="w-full">
+          <PolkadexTable.Header className="sticky top-0 bg-black">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <PolkadexTable.Row key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
                   return (
-                    <PolkadexTable.Cell
-                      key={cell.id}
-                      className={classNames("px-2 py-4 text-xs")}
+                    <PolkadexTable.Head
+                      className={classNames(
+                        "px-2 text-primary font-semibold text-xs"
+                      )}
+                      key={header.id}
                     >
                       {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
+                        header.column.columnDef.header,
+                        header.getContext()
                       )}
-                    </PolkadexTable.Cell>
+                    </PolkadexTable.Head>
                   );
                 })}
               </PolkadexTable.Row>
-            );
-          })}
-        </PolkadexTable.Body>
-      </PolkadexTable>
-    </div>
+            ))}
+          </PolkadexTable.Header>
+          <PolkadexTable.Body>
+            {table.getRowModel().rows.map((row, i) => {
+              return (
+                <PolkadexTable.Row
+                  key={row.id}
+                  className={classNames(
+                    "hover:bg-level-1 cursor-pointer",
+                    i % 2 && "bg-level-1"
+                  )}
+                >
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <PolkadexTable.Cell
+                        key={cell.id}
+                        className={classNames("px-2 py-4 text-xs")}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </PolkadexTable.Cell>
+                    );
+                  })}
+                </PolkadexTable.Row>
+              );
+            })}
+          </PolkadexTable.Body>
+        </PolkadexTable>
+      )}
+      {!isLoading && error && (
+        <div className="flex flex-col items-center justify-center gap-2">
+          <p>{error}</p>
+          <Button.Solid onClick={() => onFetchNextPage()} size="sm">
+            Try again
+          </Button.Solid>
+        </div>
+      )}
+    </InfiniteScroll>
   );
 };
