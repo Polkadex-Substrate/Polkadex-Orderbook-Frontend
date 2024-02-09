e909c7c64a0fc7bf3e1c058453dc9082a3e4ea21
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-02-09 10:19:40 +0530

    fix: range component

--- apps/hestia/src/components/ui/Temp/range.tsx
+++ apps/hestia/src/components/ui/Temp/range.tsx
@@ -3,6 +3,7 @@ import { Button, Popover, Typography } from "@polkadex/ux";
 import { Icons } from "..";
 
 interface RangeProps {
+  popover?: boolean;
   ranges: {
     value: string;
     action: () => void;
@@ -10,7 +11,7 @@ interface RangeProps {
 }
 const styles =
   "flex flex-1 flex-col items-center justify-center text-primary hover:text-textBase h-7 border border-secondary px-1 rounded duration-300 ease-in-out transition-colors";
-export const Range = ({ ranges }: RangeProps) => {
+export const Range = ({ ranges, popover = false }: RangeProps) => {
   return (
     <div className="flex flex-1 justify-between gap-2 items-center">
       {ranges.map(({ value, action }, i) => (
@@ -23,14 +24,16 @@ export const Range = ({ ranges }: RangeProps) => {
           {value}
         </Button.Outline>
       ))}
-      <Popover>
-        <Popover.Trigger className={styles}>
-          <Icons.Range className="w-6 h-4" />
-        </Popover.Trigger>
-        <Popover.Content>
-          <Typography.Text>Testing</Typography.Text>
-        </Popover.Content>
-      </Popover>
+      {popover && (
+        <Popover>
+          <Popover.Trigger className={styles}>
+            <Icons.Range className="w-6 h-4" />
+          </Popover.Trigger>
+          <Popover.Content>
+            <Typography.Text>Testing</Typography.Text>
+          </Popover.Content>
+        </Popover>
+      )}
     </div>
   );
 };
