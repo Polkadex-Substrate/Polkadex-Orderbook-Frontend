3182989bc6289947641324ce0087888b7b3e7b6a
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-02-12 22:47:52 +0530

    chore: fix types and upgrade polkadot dependencies

--- packages/core/src/helpers/getChainFromTicker.ts
+++ packages/core/src/helpers/getChainFromTicker.ts
@@ -1,14 +1,22 @@
 // Returns chain name for an asset ticker
+import { de } from "date-fns/locale";
+
 export const getChainFromTicker = (ticker: string): string => {
   // Should update it whenever any new asset is added to orderbook
-  const chains = {
-    USDT: "Asset Hub",
-    ASTR: "Astar Network",
-    DOT: "Polkadot",
-    IBTC: "Interlay",
-    PHA: "Phala Network",
-    GLMR: "Moonbeam Network",
-  };
-
-  return chains[ticker];
+  switch (ticker) {
+    case "USDT":
+      return "Asset Hub";
+    case "ASTR":
+      return "Astar Network";
+    case "DOT":
+      return "Polkadot";
+    case "IBTC":
+      return "Interlay";
+    case "PHA":
+      return "Phala Network";
+    case "GLMR":
+      return "Moonbeam Network";
+    default:
+      return "Unknown";
+  }
 };
