bcf0f94bb493b63eb27168ae1ba89266ea5fa5dc
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-02-15 15:26:12 +0530

    fix: loading next page state and pagination

--- apps/hestia/src/components/transactions/OpenOrders/index.tsx
+++ apps/hestia/src/components/transactions/OpenOrders/index.tsx
@@ -1,7 +1,7 @@
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import classNames from "classnames";
 import { useOpenOrders } from "@orderbook/core/hooks";
-import { GenericMessage, Table as PolkadexTable } from "@polkadex/ux";
+import { GenericMessage, Loading, Table } from "@polkadex/ux";
 import {
   flexRender,
   getCoreRowModel,
@@ -22,9 +22,21 @@ export const OpenOrders = forwardRef<HTMLDivElement, Props>(
   ({ maxHeight }, ref) => {
     const { onCancelOrder } = useOrders();
     const { isLoading, openOrders } = useOpenOrders();
+    const [isFetchingNextPage, setIsfetchingNext] = useState(false);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [page, setPage] = useState(1);
+
+    const onSetRowsPerPage = async (row: number) => {
+      setIsfetchingNext(true);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log(row);
+      setRowsPerPage(row);
+      setPage(1);
+      setIsfetchingNext(false);
+    };
 
     const table = useReactTable({
-      data: openOrders,
+      data: openOrders.splice(rowsPerPage * (page - 1), rowsPerPage * page),
       columns: columns({ onCancelOrder }),
       getCoreRowModel: getCoreRowModel(),
     });
@@ -42,62 +54,76 @@ export const OpenOrders = forwardRef<HTMLDivElement, Props>(
 
     return (
       <div className="flex-1 flex flex-col">
-        <div className="flex-1 flex flex-col justify-between border-b border-secondary-base">
-          <div
-            className="overflow-y-hidden hover:overflow-y-auto px-3"
-            style={{ maxHeight, scrollbarGutter: "stable" }}
-          >
-            <PolkadexTable className="w-full" even>
-              <PolkadexTable.Header className="sticky top-0 bg-backgroundBase">
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <PolkadexTable.Row key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => {
-                      return (
-                        <PolkadexTable.Head
-                          className={classNames(
-                            "px-2 text-primary font-semibold text-xs"
-                          )}
-                          key={header.id}
-                        >
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}
-                        </PolkadexTable.Head>
-                      );
-                    })}
-                  </PolkadexTable.Row>
-                ))}
-              </PolkadexTable.Header>
-              <PolkadexTable.Body>
-                {table.getRowModel().rows.map((row) => {
-                  return (
-                    <PolkadexTable.Row
-                      key={row.id}
-                      className={classNames("hover:bg-level-1 cursor-pointer")}
-                    >
-                      {row.getVisibleCells().map((cell) => {
+        <div className="flex-1 flex flex-col justify-between border-b border-secondary-base [&_svg]:scale-150">
+          <Loading.Spinner active={isFetchingNextPage}>
+            <div
+              className="overflow-y-hidden hover:overflow-y-auto px-3"
+              style={{ maxHeight, scrollbarGutter: "stable" }}
+            >
+              <Table
+                className={classNames(
+                  "w-full",
+                  isFetchingNextPage && "opacity-50"
+                )}
+                even
+              >
+                <Table.Header className="sticky top-0 bg-backgroundBase">
+                  {table.getHeaderGroups().map((headerGroup) => (
+                    <Table.Row key={headerGroup.id}>
+                      {headerGroup.headers.map((header) => {
                         return (
-                          <PolkadexTable.Cell
-                            key={cell.id}
-                            className={classNames("px-2 py-4 text-xs")}
-                          >
-                            {flexRender(
-                              cell.column.columnDef.cell,
-                              cell.getContext()
+                          <Table.Head
+                            className={classNames(
+                              "px-2 text-primary font-semibold text-xs"
                             )}
-                          </PolkadexTable.Cell>
+                            key={header.id}
+                          >
+                            {header.isPlaceholder
+                              ? null
+                              : flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext()
+                                )}
+                          </Table.Head>
                         );
                       })}
-                    </PolkadexTable.Row>
-                  );
-                })}
-              </PolkadexTable.Body>
-            </PolkadexTable>
-          </div>
-          <TablePagination ref={ref} />
+                    </Table.Row>
+                  ))}
+                </Table.Header>
+                <Table.Body>
+                  {table.getRowModel().rows.map((row) => {
+                    return (
+                      <Table.Row
+                        key={row.id}
+                        className={classNames(
+                          "hover:bg-level-1 cursor-pointer"
+                        )}
+                      >
+                        {row.getVisibleCells().map((cell) => {
+                          return (
+                            <Table.Cell
+                              key={cell.id}
+                              className={classNames("px-2 py-4 text-xs")}
+                            >
+                              {flexRender(
+                                cell.column.columnDef.cell,
+                                cell.getContext()
+                              )}
+                            </Table.Cell>
+                          );
+                        })}
+                      </Table.Row>
+                    );
+                  })}
+                </Table.Body>
+              </Table>
+            </div>
+          </Loading.Spinner>
+          <TablePagination
+            rowsPerPage={rowsPerPage}
+            onSetRowsPerPage={onSetRowsPerPage}
+            ref={ref}
+          />
         </div>
       </div>
     );
--- apps/hestia/src/components/ui/TablePagination/index.tsx
+++ apps/hestia/src/components/ui/TablePagination/index.tsx
@@ -1,41 +1,49 @@
 import { Typography, Dropdown, Pagination } from "@polkadex/ux";
 import { forwardRef } from "react";
 
-export const TablePagination = forwardRef<HTMLDivElement>((_, ref) => {
-  return (
-    <div
-      ref={ref}
-      className="bg-backgroundBase flex items-center justify-between gap-2 px-3 py-1 border-t border-primary flex-wrap"
-    >
-      <Typography.Text appearance="primary">0 of 100 row(s)</Typography.Text>
-      <div className="flex items-center gap-10">
-        <div className="flex items-center gap-2">
-          <Typography.Text>Rows per page</Typography.Text>
-          <Dropdown>
-            <Dropdown.Trigger className="border rounded-md border-secondary-base p-1">
-              10
-              <Dropdown.Icon />
-            </Dropdown.Trigger>
-            <Dropdown.Content>
-              <Dropdown.ItemCheckbox checked>10</Dropdown.ItemCheckbox>
-              <Dropdown.ItemCheckbox>20</Dropdown.ItemCheckbox>
-              <Dropdown.ItemCheckbox>30</Dropdown.ItemCheckbox>
-              <Dropdown.ItemCheckbox>40</Dropdown.ItemCheckbox>
-            </Dropdown.Content>
-          </Dropdown>
-        </div>
-        <div>
-          <Pagination>
-            <Pagination.Page currentPage={1} pagesLength={10} />
-            <Pagination.Content>
-              <Pagination.Button arrowSide="left" />
-              <Pagination.Button arrowSide="right" />
-            </Pagination.Content>
-          </Pagination>
+type Props = {
+  rowsPerPage?: number;
+  onSetRowsPerPage?: (row: number) => void;
+};
+
+export const TablePagination = forwardRef<HTMLDivElement, Props>(
+  ({ rowsPerPage, onSetRowsPerPage }, ref) => {
+    const rows = [10, 20, 30];
+    return (
+      <div
+        ref={ref}
+        className="bg-backgroundBase flex items-center justify-end gap-2 px-3 py-1 border-t border-primary flex-wrap"
+      >
+        <div className="flex items-center gap-10">
+          <div className="flex items-center gap-2">
+            <Typography.Text>Rows per page</Typography.Text>
+            <Dropdown>
+              <Dropdown.Trigger className="border rounded-md border-secondary-base p-1">
+                10
+                <Dropdown.Icon />
+              </Dropdown.Trigger>
+              <Dropdown.Content>
+                {rows.map((row, i) => (
+                  <Dropdown.ItemCheckbox key={i} checked={row === rowsPerPage}>
+                    {row}
+                  </Dropdown.ItemCheckbox>
+                ))}
+              </Dropdown.Content>
+            </Dropdown>
+          </div>
+          <div>
+            <Pagination>
+              <Typography.Text className="text-sm">Page 1</Typography.Text>
+              <Pagination.Content>
+                <Pagination.Button arrowSide="left" />
+                <Pagination.Button arrowSide="right" />
+              </Pagination.Content>
+            </Pagination>
+          </div>
         </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 TablePagination.displayName = "TablePagination";
