f0b6193a4633be203826560b5fa2b02e908fb001
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-02-15 08:53:31 -0300

    Revert "Revert "fix:  created a new SizeProvider - trading page""
    This reverts commit d123f9387a93490eb22db52712d6985638427aa0.

--- apps/hestia/src/app/trading/[id]/layout.tsx
+++ apps/hestia/src/app/trading/[id]/layout.tsx
@@ -3,6 +3,12 @@
 import { OrdersProvider } from "@orderbook/core/providers";
 import { ReactNode } from "react";
 
+import { SizeProvider } from "@/components/trading/provider";
+
 export default function Layout({ children }: { children: ReactNode }) {
-  return <OrdersProvider>{children}</OrdersProvider>;
+  return (
+    <OrdersProvider>
+      <SizeProvider>{children}</SizeProvider>
+    </OrdersProvider>
+  );
 }
