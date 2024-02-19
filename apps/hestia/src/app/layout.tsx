65dc67cf0b0c2f262fcea234a9f9b18f09ca22b6
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-02-19 22:29:21 +0530

    feat: base layout for pwa

--- apps/hestia/src/app/layout.tsx
+++ apps/hestia/src/app/layout.tsx
@@ -3,12 +3,17 @@ import "@polkadex/ux/dist/index.css";
 import { GeistSans } from "geist/font/sans";
 import { ReactNode } from "react";
 import classNames from "classnames";
+import { Head } from "next/document";
 
 import { DynamicProviders } from "@/components/ui/DynamicProviders";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="manifest" href={"/manifest.json"} />
+        <link rel={"apple-touch-icon"} href={"/icon.png"} />
+      </Head>
       <body
         className={classNames(
           "flex flex-col max-h-screen min-h-screen mx-auto border-x border-primary overflow-x-hidden",
