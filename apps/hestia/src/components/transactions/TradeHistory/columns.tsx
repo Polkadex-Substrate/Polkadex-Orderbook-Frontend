cfafd4a7652dfdd2827e899b90f52af5b6dd4f4b
Arjun Porwal <porwalarjun95@gmail.com>
2024-02-20 13:33:00 +0530

    chore: svg change

--- apps/hestia/src/components/transactions/TradeHistory/columns.tsx
+++ apps/hestia/src/components/transactions/TradeHistory/columns.tsx
@@ -1,6 +1,6 @@
 import { Trade } from "@orderbook/core/utils/orderbookService/types";
 import { createColumnHelper } from "@tanstack/react-table";
-import { DocumentDuplicateIcon } from "@heroicons/react/24/solid";
+import { DocumentDuplicateIcon } from "@heroicons/react/24/outline";
 import { Copy, Tooltip, Typography, truncateString } from "@polkadex/ux";
 
 import { filters } from "./filters";
@@ -13,7 +13,7 @@ export const columns = () => [
     cell: (e) => {
       return (
         <Copy value={e.getValue().tradeId}>
-          <div className="flex items-center gap-1">
+          <div className="flex items-center gap-2">
             <DocumentDuplicateIcon className="w-4 h-4 text-actionInput" />
             <Typography.Text size="xs">
               {truncateString(e.getValue().tradeId, 4)}
