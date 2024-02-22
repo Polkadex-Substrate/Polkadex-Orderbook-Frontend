17a31efb0239426248d46aacaae0eee9506bd7c7
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-02-22 07:09:32 -0300

    fix: graph subhead actions

--- apps/hestia/src/components/trading/Graph/header.tsx
+++ apps/hestia/src/components/trading/Graph/header.tsx
@@ -1,7 +1,6 @@
 import { ArrowsPointingOutIcon } from "@heroicons/react/24/solid";
-import classNames from "classnames";
 import { CameraIcon } from "@heroicons/react/24/outline";
-import { Tabs } from "@polkadex/ux";
+import { Button, Tabs, Typography } from "@polkadex/ux";
 
 import { ResolutionString } from "../../../../public/static/charting_library/charting_library";
 
@@ -19,26 +18,25 @@ export const Header = ({
   onScreenshot: () => void;
 }) => {
   return (
-    <div className="flex gap-2 items-center flex-wrap justify-between border-b border-primary px-3 py-2 bg-level-0">
+    <div className="flex gap-2 items-center flex-wrap justify-between border-b border-primary px-3 py-1 bg-level-0">
       <div className="flex items-center gap-3">
-        <span className="text-primary">Time</span>
+        <Typography.Text appearance="secondary">Time</Typography.Text>
         <ul className="flex items-center gap-2">
           {supported_resolutions.map((v) => {
             const active = v.id === activeResolution;
             return (
-              <li
-                className={classNames(
-                  active
-                    ? "text-primary-base"
-                    : "text-primary hover:text-secondary duration-300 transition-colors",
-                  "font-semibold text-sm"
-                )}
-                key={v.id}
-              >
+              <li key={v.id}>
                 <button
+                  type="button"
                   onClick={() => onChangeResolution(v.id as ResolutionString)}
                 >
-                  {v.description}
+                  <Typography.Text
+                    size="xs"
+                    appearance={active ? "primary-base" : "primary"}
+                    bold={active}
+                  >
+                    {v.description}
+                  </Typography.Text>
                 </button>
               </li>
             );
@@ -52,19 +50,23 @@ export const Header = ({
             Original Chart
           </Tabs.Trigger>
         </Tabs.List>
-        <div className="flex gap-2">
-          <button
+        <div className="flex gap-1">
+          <Button.Icon
+            size="sm"
+            className="p-1 text-primary"
+            variant="ghost"
             onClick={onScreenshot}
-            className="hover:bg-level-3 duration-300 transition-colors group"
           >
-            <CameraIcon className="w-5 h-5 text-secondary group-hover:text-primary-base duration-300 transition-colors" />
-          </button>
-          <button
+            <CameraIcon />
+          </Button.Icon>
+          <Button.Icon
+            size="sm"
+            className="p-1 text-primary"
+            variant="ghost"
             onClick={onChangeFullScreen}
-            className="hover:bg-level-3 duration-300 transition-colors group"
           >
-            <ArrowsPointingOutIcon className="w-5 h-5 text-secondary group-hover:text-primary-base duration-300 transition-colors" />
-          </button>
+            <ArrowsPointingOutIcon />
+          </Button.Icon>
         </div>
       </div>
     </div>
