f8cceadeed0f1133a4d295f5f957d2e69157f674
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-02-24 17:42:46 -0300

    fix: removed unused component

--- apps/hestia/src/components/trading/Trades/index.tsx
+++ apps/hestia/src/components/trading/Trades/index.tsx
@@ -19,28 +19,11 @@ export const Trades = ({
         className="flex h-full flex-col overflow-hidden"
         style={{ height: maxHeight }}
       >
-        <div className="flex items-center justify-between border-b border-primary">
+        <div className="flex border-b border-primary">
           <Tabs.List className="px-2 py-2.5">
             <Tabs.Trigger value="markets">Markets</Tabs.Trigger>
             <Tabs.Trigger value="recentTrades">Recent Trades</Tabs.Trigger>
           </Tabs.List>
-          <Dropdown>
-            <Dropdown.Trigger>
-              <div className="flex justify-center items-center w-[1.8rem] h-[1.8rem] p-[0.25rem] hover:bg-level-1 transition-colors duration-300 rounded opacity-100">
-                <EllipsisVerticalIcon />
-              </div>
-            </Dropdown.Trigger>
-            <Dropdown.Content>
-              {["Example1", "Example2"].map((value, i) => (
-                <Dropdown.Item
-                  key={i}
-                  onClick={() => window.alert("Changing.")}
-                >
-                  {value}
-                </Dropdown.Item>
-              ))}
-            </Dropdown.Content>
-          </Dropdown>
         </div>
         <Tabs.Content value="markets" className="bg-level-0">
           <Markets />
