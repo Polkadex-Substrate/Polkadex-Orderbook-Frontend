a40434177f24d37b8e48b2eb302dd2bae717a377
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-02-29 11:21:20 +0530

    fix: height of trades section

--- apps/hestia/src/components/trading/Trades/index.tsx
+++ apps/hestia/src/components/trading/Trades/index.tsx
@@ -16,7 +16,7 @@ export const Trades = ({
     <Tabs defaultValue="markets" className="flex-initial max-xl:flex-1 h-full">
       <div
         className="flex-1 flex h-full flex-col overflow-hidden"
-        style={{ height: maxHeight }}
+        style={{ maxHeight }}
       >
         <div className="flex border-b border-primary">
           <Tabs.List className="px-2 py-2.5">
