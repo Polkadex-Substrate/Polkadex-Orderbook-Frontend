ccb100f3fef64db6f39358a16e56334699bbdc98
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-03-06 21:13:53 -0300

    fix: image max-w

--- apps/hestia/src/components/rewardsLanding/callToAction.tsx
+++ apps/hestia/src/components/rewardsLanding/callToAction.tsx
@@ -8,8 +8,8 @@ import OrderbookImage from "../../../public/img/obHestia.svg";
 export const CallToAction = () => {
   return (
     <div className="relative max-lg:flex-col flex items-center justify-between lg:gap-8 max-md:px-4 pl-10 border border-primary rounded-md my-10 overflow-hidden">
-      <div className="flex flex-col gap-5 flex-1 max-w-[350px] max-lg:py-10 max-lg:text-center max-lg:items-center">
-        <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-6 flex-1 max-w-[350px] max-lg:py-10 max-lg:text-center max-lg:items-center">
+        <div className="flex flex-col gap-2">
           <Typography.Text size="xl" appearance="primary">
             Your keys, your crypto, your exchange
           </Typography.Text>
@@ -31,7 +31,7 @@ export const CallToAction = () => {
           width: "100%",
           height: "100%",
         }}
-        className="flex-1 lg:border-[1px] border-primary max-lg:max-w-[600px] lg:max-w-[700px] lg:mt-10"
+        className="flex-1 lg:border-[1px] border-primary max-lg:max-w-[600px] lg:max-w-[650px] lg:mt-10"
       />
     </div>
   );
