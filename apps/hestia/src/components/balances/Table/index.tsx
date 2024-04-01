faabe3c2e52445429b6119ab1a274e79920c4d9f
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-04-01 09:23:19 -0300

    Internal to release  (#1170)
    * Unexpected Balance Format Issue When Clicking the Max Button (#1168)
    
    * fix: validations
    
    * fix: validation
    
    * update: remove comments
    
    * Feedback Improvements <> Orderbook Frontend (#1169)
    
    * fix: feedbacks
    
    * fix: default desc order of balances table
    
    ---------
    
    Co-authored-by: Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>

--- apps/hestia/src/components/balances/Table/index.tsx
+++ apps/hestia/src/components/balances/Table/index.tsx
@@ -1,5 +1,3 @@
-// TODO: Add generic empty assets
-
 import { Table as PolkadexTable, GenericMessage } from "@polkadex/ux";
 import { useWindowSize } from "usehooks-ts";
 import { Fragment, forwardRef, useEffect, useMemo, useState } from "react";
@@ -11,21 +9,25 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { Asset } from "@orderbook/core/API";
 import classNames from "classnames";
 
-// import { ResponsiveData } from "./responsiveData";
 import { columns } from "./columns";
 import { Loading } from "./loading";
+import { ResponsiveTable } from "./responsiveTable";
 const responsiveKeys = ["inOrders", "fundingAccount"];
 
 export const Table = forwardRef<
   HTMLDivElement,
   { maxHeight: string; data: AssetsProps[]; loading: boolean }
->(({ maxHeight, data, loading }, ref) => {
-  const [state, setState] = useState<Asset | null>(null);
+>(({ maxHeight, data, loading }) => {
+  const [responsiveState, setResponsiveState] = useState(false);
+  const [responsiveData, setResponsiveData] = useState<AssetsProps | null>(
+    null
+  );
 
-  const [sorting, setSorting] = useState<SortingState>([]);
+  const [sorting, setSorting] = useState<SortingState>([
+    { desc: true, id: "fundingAccount" },
+  ]);
 
   const { width } = useWindowSize();
 
@@ -41,18 +43,21 @@ export const Table = forwardRef<
   const responsiveView = useMemo(() => width <= 800, [width]);
 
   useEffect(() => {
-    if (!responsiveView && !!state) setState(null);
-  }, [responsiveView, state]);
+    if (!responsiveView && !!responsiveState) {
+      setResponsiveState(false);
+      setResponsiveData(null);
+    }
+  }, [responsiveState, responsiveView]);
 
   if (loading) return <Loading />;
 
   return (
     <Fragment>
-      {/* <ResponsiveData
-        open={!!state}
-        onClose={() => setState(null)}
-        data={state}
-      /> */}
+      <ResponsiveTable
+        data={responsiveData}
+        onOpenChange={setResponsiveState}
+        open={responsiveState}
+      />
       {data?.length ? (
         <div className="flex-1 flex flex-col justify-between border-b border-secondary-base bg-level-0">
           <div
@@ -108,10 +113,21 @@ export const Table = forwardRef<
                         )
                           return null;
 
+                        const responsiveProps = responsiveView
+                          ? {
+                              className: "cursor-pointer",
+                              onClick: () => {
+                                setResponsiveState(true);
+                                setResponsiveData(row.original);
+                              },
+                            }
+                          : {};
+
                         return (
                           <PolkadexTable.Cell
                             key={cell.id}
                             align={i === 0 ? "left" : "right"}
+                            {...responsiveProps}
                           >
                             {flexRender(
                               cell.column.columnDef.cell,
--- packages/core/src/helpers/Utils.ts
+++ packages/core/src/helpers/Utils.ts
@@ -86,3 +86,39 @@ export const secondsToHm = (seconds: number) => {
   if (dhm.trim().length === 0) return sDisplay;
   return dhm;
 };
+
+// * Credit goes to https://github.com/balancer/balancer-v2-monorepo/blob/8b5773510dfc7a94d4eef3e22d1de50becb0250d/lib/helpers/numbers.ts#L57-L86 for providing the solution.
+export function parseScientific(num: string): string {
+  // If the number is not in scientific notation return it as it is.
+  if (!/\d+\.?\d*e[+-]*\d+/i.test(num)) {
+    return num;
+  }
+
+  // Remove the sign.
+  const numberSign = Math.sign(Number(num));
+  num = Math.abs(Number(num)).toString();
+
+  // Parse into coefficient and exponent.
+  const [coefficient, exponent] = num.toLowerCase().split("e");
+  let zeros = Math.abs(Number(exponent));
+  const exponentSign = Math.sign(Number(exponent));
+  const [integer, decimals] = (
+    coefficient.indexOf(".") !== -1 ? coefficient : `${coefficient}.`
+  ).split(".");
+
+  if (exponentSign === -1) {
+    zeros -= integer.length;
+    num =
+      zeros < 0
+        ? integer.slice(0, zeros) + "." + integer.slice(zeros) + decimals
+        : "0." + "0".repeat(zeros) + integer + decimals;
+  } else {
+    if (decimals) zeros -= decimals.length;
+    num =
+      zeros < 0
+        ? integer + decimals.slice(0, zeros) + "." + decimals.slice(zeros)
+        : integer + decimals + "0".repeat(zeros);
+  }
+
+  return numberSign < 0 ? "-" + num : num;
+}
