4769b795ef818b12da43ba2ed36e6a27b8fd9b80
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-05-08 07:33:03 -0300

    fix: types

--- packages/core/src/providers/user/thea/index.tsx
+++ packages/core/src/providers/user/thea/index.tsx
@@ -194,7 +194,7 @@ export const TheaProvider = ({
     sourceAddress: sourceAccountSelected?.address ?? "",
     assets: sourceAssets,
     chains,
-    selectedChain: sourceChain?.genesis,
+    selectedChain: sourceChain?.genesis ?? "",
   });
 
   return (
