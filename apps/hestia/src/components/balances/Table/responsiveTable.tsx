69bd53e86c1647fbaee52ef8c7d8a6fcab8bf099
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-05-17 07:26:18 -0300

    fix: remove unused changes

--- apps/hestia/src/components/balances/Table/responsiveTable.tsx
+++ apps/hestia/src/components/balances/Table/responsiveTable.tsx
@@ -1,4 +1,4 @@
-import { Button, Drawer, TokenAppearance } from "@polkadex/ux";
+import { Button, Drawer, Tokens } from "@polkadex/ux";
 import { Dispatch, SetStateAction } from "react";
 import { AssetsProps } from "@orderbook/core/hooks";
 import Link from "next/link";
@@ -30,7 +30,7 @@ export const ResponsiveTable = ({
           <TokenCard
             tokenName={chainName}
             ticker={ticker}
-            icon={ticker as TokenAppearance}
+            icon={ticker as keyof typeof Tokens}
           />
         </ResponsiveCard>
         <ResponsiveCard label="Funding account">
