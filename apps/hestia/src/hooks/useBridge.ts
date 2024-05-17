1edf2536199be4629ef6e31e4cc7904f69b56dfd
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-05-17 08:51:35 -0300

    fix: useBridge

--- apps/hestia/src/hooks/useBridge.ts
+++ apps/hestia/src/hooks/useBridge.ts
@@ -1,11 +1,13 @@
+// TODO: Move messages
+
 import { useTheaProvider } from "@orderbook/core/providers";
 import { SubmittableExtrinsic } from "@polkadot/api/promise/types";
 import { useMutation } from "@tanstack/react-query";
 import { useSettingsProvider } from "@orderbook/core/providers/public/settings";
 import { signAndSendExtrinsic, sleep } from "@orderbook/core/helpers";
 import { useNativeApi } from "@orderbook/core/providers/public/nativeApi";
+import { signAndSubmitPromiseWrapper } from "@polkadex/blockchain-api";
 
-// TEMP
 const withdrawMessage =
   "After withdrawal initiation, expect tokens on the destination chain in 2-3 minutes";
 const depositMessage =
@@ -21,31 +23,35 @@ export function useBridge({ onSuccess }: { onSuccess: () => void }) {
     onRefreshTransactions,
     onRefetchSourceBalances,
   } = useTheaProvider();
+
   return useMutation({
     mutationFn: async ({ amount }: { amount: number }) => {
-      // const decimales =
-      //   networks.find((x) => x.id === transferConfig?.sourceChain.genesis)
-      //     ?.decimals ?? 12;
-      // const formatedAmount = toPlanck(amount, decimales).toNumber();
-
       if (!transferConfig || !sourceAccount || !api) {
         onHandleError?.("Bridge issue");
         return;
       }
 
       const ext = await transferConfig.transfer<SubmittableExtrinsic>(amount);
+      if (isPolkadexChain)
+        await signAndSendExtrinsic(
+          api,
+          ext,
+          { signer: sourceAccount.signer },
+          sourceAccount.address,
+          true
+        );
+      else
+        await signAndSubmitPromiseWrapper({
+          signer: sourceAccount.signer,
+          tx: ext,
+          address: sourceAccount.address,
+          criteria: "IS_FINALIZED",
+        });
 
-      await signAndSendExtrinsic(
-        api,
-        ext,
-        { signer: sourceAccount.signer },
-        sourceAccount.address,
-        true
-      );
       onSuccess();
       onHandleAlert(isPolkadexChain ? withdrawMessage : depositMessage);
       if (isPolkadexChain) {
-        await sleep(3000);
+        await sleep(4000);
         await onRefreshTransactions();
       }
       await onRefetchSourceBalances();
