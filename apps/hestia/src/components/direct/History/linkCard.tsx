5d962e3c92b23e1eaed3c9d27648b09344ff75ea
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-06-19 12:06:15 +0900

    fix: link noopener

--- apps/hestia/src/components/direct/History/linkCard.tsx
+++ apps/hestia/src/components/direct/History/linkCard.tsx
@@ -7,7 +7,7 @@ export const LinkCard = ({ value = "" }: { value?: string }) => {
   const shortData = truncateString(value);
   const href = `https://polkadex.subscan.io/block/${value}`;
   return (
-    <Link target="_blank" href={href}>
+    <Link target="_blank" rel="noopener noreferrer" href={href}>
       <div className="flex items-center gap-1">
         <RiExternalLinkLine className="w-3.5 h-3.5 text-actionInput" />
         <Typography.Text appearance="primary" size="sm">
