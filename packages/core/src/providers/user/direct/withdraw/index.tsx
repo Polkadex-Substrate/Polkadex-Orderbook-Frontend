4d53c3ef8ddbf5c7842066363fb9be718cc9d605
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-06-23 21:17:30 +0530

    feat: source chain logic in withdraw provider

--- packages/core/src/providers/user/direct/withdraw/index.tsx
+++ packages/core/src/providers/user/direct/withdraw/index.tsx
@@ -1,14 +1,50 @@
-import { PropsWithChildren, createContext } from "react";
+import { GENESIS } from "@orderbook/core/constants";
+import { Asset, Chain, Thea } from "@polkadex/thea";
+import { PropsWithChildren, createContext, useMemo, useState } from "react";
+import { ExtensionAccount } from "@polkadex/react-providers";
+
+import { useConnectWalletProvider } from "../../connectWalletProvider";
 
 export { useDirectWithdrawProvider } from "./useDirectWithdrawProvider";
 
 export const DirectWithdrawProvider = ({ children }: PropsWithChildren) => {
-  return <Provider value={{}}>{children}</Provider>;
+  const { getAllChains } = useMemo(() => new Thea(), []);
+  const [selectedAsset, setSelectedAsset] = useState<Asset | null>(null);
+
+  const { selectedWallet } = useConnectWalletProvider();
+
+  const chains = useMemo(() => getAllChains(), [getAllChains]);
+
+  /* Source Chain (which would be Polkadex always) */
+  const sourceChain = useMemo(
+    () => chains.find((c) => c.genesis === GENESIS[0]) as Chain,
+    [chains]
+  );
+
+  const sourceAccountSelected = useMemo(() => selectedWallet, [selectedWallet]);
+
+  return (
+    <Provider
+      value={{ chains, sourceChain, sourceAccount: sourceAccountSelected }}
+    >
+      {children}
+    </Provider>
+  );
 };
 
-type State = {{}};
+type State = {
+  chains: Chain[];
 
-const Context = createContext<State>({});
+  sourceChain: Chain | null;
+  sourceAccount?: ExtensionAccount;
+};
+
+const Context = createContext<State>({
+  chains: [],
+
+  sourceChain: null,
+  sourceAccount: undefined,
+});
 
 const Provider = ({ value, children }: PropsWithChildren<{ value: State }>) => {
   return <Context.Provider value={value}>{children}</Context.Provider>;
