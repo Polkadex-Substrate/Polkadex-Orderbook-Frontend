aa1ecff4dd95d2673de0d2a7b353e5bf6609503f
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-06-23 21:27:07 +0530

    feat: destination chain logic in withdraw provider

--- packages/core/src/providers/user/direct/withdraw/index.tsx
+++ packages/core/src/providers/user/direct/withdraw/index.tsx
@@ -1,6 +1,19 @@
 import { GENESIS } from "@orderbook/core/constants";
-import { Asset, Chain, Thea } from "@polkadex/thea";
-import { PropsWithChildren, createContext, useMemo, useState } from "react";
+import {
+  Asset,
+  Chain,
+  ChainType,
+  Thea,
+  getChainConnector,
+} from "@polkadex/thea";
+import {
+  Dispatch,
+  PropsWithChildren,
+  SetStateAction,
+  createContext,
+  useMemo,
+  useState,
+} from "react";
 import { ExtensionAccount } from "@polkadex/react-providers";
 
 import { useConnectWalletProvider } from "../../connectWalletProvider";
@@ -9,6 +22,9 @@ export { useDirectWithdrawProvider } from "./useDirectWithdrawProvider";
 
 export const DirectWithdrawProvider = ({ children }: PropsWithChildren) => {
   const { getAllChains } = useMemo(() => new Thea(), []);
+  const [destinationChain, setDestinationChain] = useState<Chain | null>(null);
+  const [destinationAccount, setDestinationAccount] =
+    useState<ExtensionAccount>();
   const [selectedAsset, setSelectedAsset] = useState<Asset | null>(null);
 
   const { selectedWallet } = useConnectWalletProvider();
@@ -21,11 +37,49 @@ export const DirectWithdrawProvider = ({ children }: PropsWithChildren) => {
     [chains]
   );
 
+  const sourceConnector = useMemo(
+    () => sourceChain && getChainConnector(sourceChain.genesis),
+    [sourceChain]
+  );
+
   const sourceAccountSelected = useMemo(() => selectedWallet, [selectedWallet]);
+
+  /* Destination Chain */
+  const destinationAccountSelected = useMemo(
+    () =>
+      destinationAccount ??
+      (destinationChain?.type === ChainType.Substrate
+        ? selectedWallet
+        : undefined),
+    [destinationAccount, selectedWallet, destinationChain?.type]
+  );
+
+  const onSelectDestinationChain = (chain: Chain) => {
+    if (destinationChain?.type !== chain.type) setDestinationAccount(undefined);
+    const selectedAsset = sourceConnector.getSupportedAssets(chain)[0];
+    setDestinationChain(chain);
+    setSelectedAsset(selectedAsset);
+  };
+
+  const isDestinationPolkadex = useMemo(
+    () => !!(destinationChain?.genesis === GENESIS[0]),
+    [destinationChain?.genesis]
+  );
 
   return (
     <Provider
-      value={{ chains, sourceChain, sourceAccount: sourceAccountSelected }}
+      value={{
+        chains,
+
+        sourceChain,
+        sourceAccount: sourceAccountSelected,
+
+        destinationChain,
+        onSelectDestinationChain,
+        isDestinationPolkadex,
+        destinationAccount: destinationAccountSelected,
+        setDestinationAccount,
+      }}
     >
       {children}
     </Provider>
@@ -37,6 +91,12 @@ type State = {
 
   sourceChain: Chain | null;
   sourceAccount?: ExtensionAccount;
+
+  destinationChain: Chain | null;
+  onSelectDestinationChain: (chain: Chain) => void;
+  isDestinationPolkadex: boolean;
+  destinationAccount?: ExtensionAccount;
+  setDestinationAccount: Dispatch<SetStateAction<ExtensionAccount | undefined>>;
 };
 
 const Context = createContext<State>({
@@ -44,6 +104,12 @@ const Context = createContext<State>({
 
   sourceChain: null,
   sourceAccount: undefined,
+
+  destinationChain: null,
+  onSelectDestinationChain: () => {},
+  isDestinationPolkadex: false,
+  destinationAccount: undefined,
+  setDestinationAccount: () => {},
 });
 
 const Provider = ({ value, children }: PropsWithChildren<{ value: State }>) => {
