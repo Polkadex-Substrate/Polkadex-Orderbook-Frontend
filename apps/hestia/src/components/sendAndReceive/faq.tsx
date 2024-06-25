d6eadfba6ca4bc4bfb83aa4a6fb73e02e6302785
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-06-25 11:52:49 +0530

    fix: faq

--- apps/hestia/src/components/sendAndReceive/faq.tsx
+++ apps/hestia/src/components/sendAndReceive/faq.tsx
@@ -5,18 +5,15 @@ import React from "react";
 
 export const Faq = () => {
   return (
-    <div className="flex flex-col gap-4 max-md:py-4 md:py-6 px-4">
+    <div className="flex flex-col gap-4 max-md:pt-4 md:pt-6 px-4 max-h-[440px] overflow-scroll scrollbar-hide">
       <Typography.Heading type="h2">FAQ</Typography.Heading>
-      <div className="flex flex-col gap-2 md:max-w-[450px]">
+      <div className="flex flex-col gap-2 w-[350px]">
         <Accordion
           type="multiple"
           defaultValue={["deposit"]}
           className="flex flex-col gap-4"
         >
-          <Accordion.Item
-            value="deposit"
-            className="border-b border-primary pb-3"
-          >
+          <Accordion.Item value="deposit" className="pb-3">
             <Accordion.Trigger>
               <Typography.Text appearance="primary">
                 What does Deposit means ?
@@ -30,10 +27,34 @@ export const Faq = () => {
               </Typography.Paragraph>
             </Accordion.Content>
           </Accordion.Item>
-          <Accordion.Item
-            value="withdraw"
-            className="border-b border-primary pb-3"
-          >
+          <Accordion.Item value="depositFromMoonbeam" className="pb-3">
+            <Accordion.Trigger>
+              <Typography.Text className="text-left" appearance="primary">
+                What can I deposit GLMR from Moonbeam to Polkadex Orderbook ?
+              </Typography.Text>
+              <Accordion.Icon />
+            </Accordion.Trigger>
+            <Accordion.Content className="mt-3">
+              <Typography.Paragraph className="leading-6" size="sm">
+                In order to deposit GLMR from Moonbeam network to Polkadex
+                orderbook, you have to follow these steps -
+                <ol className="mt-2">
+                  <li>
+                    1. Choose Moonbeam in &quot;From Network&quot; dropdown.
+                  </li>
+                  <li>
+                    2. Choose EVM account in &quot;From Account&quot; dropdown.
+                  </li>
+                  <li>
+                    3. Enter the amount which you need to Deposit i.e. 100 GLMR
+                  </li>
+                  <li>4. Click on Deposit</li>
+                  <li>5. Verify the details and sign transaction.</li>
+                </ol>
+              </Typography.Paragraph>
+            </Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item value="withdraw" className="pb-3">
             <Accordion.Trigger>
               <Typography.Text appearance="primary">
                 What does Withdraw means ?
